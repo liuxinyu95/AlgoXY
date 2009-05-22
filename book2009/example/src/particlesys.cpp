ecc02bdafb7301c06b1663cdb35df39dcc609688
unknown <liuuuxin@.ap.tieto.com>
2009-05-22 09:04:33 +0800

    implementing scheduler

--- book2009/example/src/particlesys.cpp
+++ book2009/example/src/particlesys.cpp
@@ -53,28 +53,52 @@ public:
     return infected==p.infected && location == p.location;
   }
 
+    void set_location(const physics& l){
+        location=l;
+    }
+
 private:
   bool infected;
   physics location;
 };
 
 class scheduler{
+public:
+    static scheduler& inst(){
+        static scheduler _inst;
+        return _inst;
+    }
+
+    void setup(int w, int h, int n){
+        a=area(w, h);
+        people=Population(n-1);
+        people.push_back(person(INFECTED));
+        put_people(people, a);
+    }
+    void start(){
+        run();
+    }
+private:
+    void run(){
+        a.reset();
+        for
+    }
+
+    area a;
+    typedef std::vector<person> Population;
+    Population people;
 };
 
 template<class Coll>
 void put_people(Coll& coll, area& a){
   for(Coll::iterator it=coll.begin(); it!=coll.end(); ++it){
-
-    int x=rand() % a.width();
-    int y=rand() % a.height();
-    int speed=rand() % MAX_SPEED;
-    int direction=rand() % 360;
+      it->set_location(physics(rand()%a.width(), rand()%a.height(),
+                               rand()%MAX_SPEED, rand()%360));
   }
 }
 
 int main(int argc, char** argv){
-  area a(1000,1000); //Beijing has people density as 888 persons/km^2, ==> 1061 m^2 is the best fit
-  std::vector<person> coll(1000-1);
-  coll.push_back(person(INFECTED));
-  put_people(coll, a);
+  //Beijing has people density as 888 persons/km^2, ==> 1061 m^2 is the best fit
+    scheculer::inst().setup(1061, 1061, 1000);
+    scheduler::inst().start();
 }
