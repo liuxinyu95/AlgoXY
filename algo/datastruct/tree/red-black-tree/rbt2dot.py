ebaa959a6a9e72396f6601d46e1e297eeaea3c92
unknown <liuuuxin@.ap.tieto.com>
2009-08-20 14:24:49 +0800

    rbt2dot in basically finished. but need add middle nil node to make the tree balanced.

--- algo/datastruct/tree/red-black-tree/rbt2dot.py
+++ algo/datastruct/tree/red-black-tree/rbt2dot.py
@@ -10,26 +10,87 @@ import getopt
 
 class Node:
     def __init__(self):
-        self.key = "."; #nil
-        self.color = "B";
-        self.left = self.right = None
+        self.key = ""
+        self.color = "";
+        self.left = self.right =None
 
 def str_to_tree(ts):
-    ts=ts.lstip()
-    if ts==".":
-        return (None, ts[1:])
+    ts=ts.lstrip()
     x = Node()
-    start=False
+    states=["parsing-key", "parsing-color"]
+    state=""
     while True:
         c=ts[0]
-        if c =='(':
-            start = True
-            (x.left, ts)=str_to_tree(ts[1:])
-            ts
-            
-            
-def tree_to_dot():
+        ts=ts[1:]
+        if c== '.':
+            return (None, ts)
+        elif c =='(':
+            (x.left, ts)=str_to_tree(ts)
+            state = "parsing-key"
+        elif c==' ':
+            if state=="parsing-color":
+                (x.right, ts)=str_to_tree(ts)
+        elif c==')':
+            return (x, ts)
+        elif c==':':
+            state="parsing-color"
+        else:
+            if state=="parsing-key":
+                x.key=x.key+c
+            elif state=="parsing-color":
+                x.color=x.color+c
+            else:
+                print "[parsing error, state=", state, " c=", c,"]"
+                sys.exit()
+
+def define_connection(node, prefix):
+    (left, leftstyle, right, rightstyle, res)=("", "\n", "", "\n", "\t")
+    if node.left is None:
+        left="nil"+prefix+node.key+"l"
+        leftstyle="[style=invis];\n"
+    else:
+        left=prefix+node.left.key
+    if node.right is None:
+        right="nil"+prefix+node.key+"r"
+        rightstyle="[style=invis];\n"
+    else:
+        right=prefix+node.right.key
+    res=res+prefix+node.key + "->" + left + leftstyle
+    res=res+"\t"+prefix+node.key + "->" + right + rightstyle
+    res=res+"\t{rank=same "+left+"->"+right+ "[style=invis]}\n"
+    return res
+
+def define_node(node, prefix="a", parent=None, postfix=""):
+    res="\t"
+    if node is None:
+        res=res+"nil"+prefix+parent.key+postfix+"[label=\"\", style=invis];\n"
+    else:
+        color_dict = {"R":"fillcolor=lightgray, fontcolor=black", 
+                      "B":"fillcolor=black, fontcolor=white"}
+        res=res+prefix+node.key+"[label=\""+node.key+"\", style=filled, "+color_dict[node.color]+"];\n"
+        res=res+define_node(node.left, prefix, node, "l")+define_node(node.right, prefix, node, "r")
+        res=res+define_connection(node, prefix)
+    return res
+
+def define_tree():
     pass
+            
+def tree_to_dot(t, filename):
+    res="digraph G{\n" "\tnode[shape=circle]\n"
+    res=res+define_node(t)
+    res=res+"}"
+    print res
+    f=open(filename,'w')
+    f.write(res)
+    f.close()
+    print "write to file ", filename, "OK."
+
+# helper for verfiy the parser
+def rbtree_to_str(t):
+    if t is None:
+        return "."
+    else:
+        return "("+rbtree_to_str(t.left)+ " " + t.key +":"+t.color+" " + rbtree_to_str(t.right)+")"
 
 def get_args(argv):
     try:
@@ -49,8 +110,9 @@ def usage():
 
 def main(argv):
     (filename, ts)=get_args(argv)
-    print filename
-    print ts
+    (t, ts)=str_to_tree(ts)
+    print "write dot for: ", rbtree_to_str(t)
+    tree_to_dot(t, filename)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
