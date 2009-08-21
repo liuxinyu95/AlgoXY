d2b222136788b7be2339ee8fac4e3fc25b4c88a0
liuxinyu.tieto <xinyu.liu@tieto.com>
2009-08-21 10:19:18 +0800

    insertion part haskell and python version written in latex.

---
+++ algo/datastruct/tree/red-black-tree/create-image.sh
@@ -0,0 +1,3 @@
+#/usr/bin/sh
+#./rbt2dot.py -o img/clrs-fig.13.4.dot "(((. 1:B .) 2:R ((. 5:R .) 7:B (. 8:R .))) 11:B (. 14:B (. 15:R .)))"
+./rbt2dot.py -o img/python-insert.dot "(((. 1:B .) 2:R ((. 3:R .) 4:B .)) 5:B ((. 6:B .) 7:R ((. 8:R .) 9:B .)))"
