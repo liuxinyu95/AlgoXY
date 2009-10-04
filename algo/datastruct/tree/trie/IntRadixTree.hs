b3581df3a13350fd87b583730925b68e009ee109
liuxinyu-home <liuxinyu95@gmail.com>
2009-10-04 08:27:43 +0800

    Little Endian Int Radix Trie initialized.
    next: programming look up for it; programming patricia trie for it; then change it to big endian.

---
+++ algo/datastruct/tree/trie/IntRadixTree.hs
@@ -0,0 +1,65 @@
+-- Int (as key) Radix tree
+-- Referred from Haskell packages Data.IntMap
+-- Other reference includes:
+--  [1] CLRS, Problems 12-2: Radix trees
+--  [2] Chris Okasaki and Andy Gill,  \"/Fast Mergeable Integer Maps/\",
+--	Workshop on ML, September 1998, pages 77-86,
+--	<http://www.cse.ogi.edu/~andy/pub/finite.htm>
+
+import Data.Bits
+
+-- 1. Very simple int (binary) trie as CLRS 12-2 (Little-Edian)
+data IntTrie a = Empty 
+               | Branch (IntTrie a) (Maybe a) (IntTrie a) -- left, value, right
+
+type Key = Int
+
+-- helpers
+left :: IntTrie a -> IntTrie a
+left (Branch l _ _) = l
+left Empty = Empty
+
+right :: IntTrie a -> IntTrie a
+right (Branch _ _ r) = r
+right Empty = Empty
+
+value :: IntTrie a -> Maybe a
+value (Branch _ v _) = v
+value Empty = Nothing
+
+-- 1.1 insert
+-- if user insert a value already binding with existed key, just over write 
+-- the previous value
+-- usage: insert trie key x
+insert :: IntTrie a -> Key -> a -> IntTrie a
+insert t 0 x = Branch (left t) (Just x) (right t)
+insert t k x = if testBit k 0 
+               then Branch (left t) (value t) (insert (right t) (k `div` 2) x)
+               else Branch (insert (left t) (k `div` 2) x) (value t) (right t)
+
+-- 1.2 look up
+--lookup :: Key -> a
+
+
+-- 1.3 test helper
+fromList :: [(Key, a)] -> IntTrie a
+fromList xs = foldl ins Empty xs where
+    ins t (k, v) = insert t k v
+
+-- k = ... a2, a1, a0 ==> k' = ai * m + k, where m=2^i
+toString :: (Show a)=>IntTrie a -> String
+toString t = toStr t 0 1 where
+    toStr Empty k m = "."
+    toStr tr k m = "(" ++ (toStr (left tr) k (2*m)) ++
+                      " " ++ (show k) ++ (valueStr (value tr)) ++
+                      " " ++ (toStr (right tr) (m+k) (2*m)) ++ ")"
+    valueStr (Just x) = ":" ++ (show x)
+    valueStr _ = ""
+
+-- 1.4 test cases
+testIntTrie = toString (fromList [(1, 'x'), (4, 'y'), (5, 'z')])
+
+-- 2. Int Patricia (Radix) tree
+
+main = do
+  putStrLn testIntTrie
