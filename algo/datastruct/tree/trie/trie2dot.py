169d4b420db9ceb291eaf19556dc82f7971dcf85
unknown <liuuuxin@.ap.tieto.com>
2009-10-14 14:34:27 +0800

    updated trie.py and trie2dot.py so that we can insert trie from map like: "110:x, 101:y, 001:z"

--- algo/datastruct/tree/trie/trie2dot.py
+++ algo/datastruct/tree/trie/trie2dot.py
@@ -2,8 +2,9 @@
 """
 transform the trie to dot script (graphviz)
 usage: 
-trie2dot.py -o filename.dot -t "110, 0, 01, 01101"
-trie2dot.py -o foo.dot -t "a, an, the, another, hello"
+trie2dot.py -o filename.dot "110, 0, 01, 01101"
+trie2dot.py -o example.dot "001:y, 100:x, 101:z"
+trie2dot.py -o foo.dot -t patricia "a, an, the, another, hello"
   o: output filename
   t: type, can be trie, patricia, suffix-trie, suffix-treex
 """
@@ -20,7 +21,10 @@ def define_node(node, prefix=""):
     res="\t"
     node_name = lambda x: "t"+x
     to_str = lambda x: "%s" %x
-    res += node_name(prefix)+"[label=\""+node.value+"\"];\n"
+    node_attrib = "[label=\"\"];\n"
+    if not node.value is None:
+        node_attrib = "[label=\""+prefix + ":" + node.value + "\", shape=ellipse];\n"
+    res += node_name(prefix)+node_attrib
     for k, v in node.children.items():
         res += define_node(v, prefix+to_str(k))
         res += define_connection(node_name(prefix), node_name(prefix+to_str(k)), to_str(k))
@@ -55,10 +59,23 @@ def usage():
     print __doc__
     sys.exit()
 
+def is_map(s):
+    return not s.find(":") == -1
+
+def to_map(s):
+    m={}
+    for elem in s.split(", "):
+        (k, v) = elem.split(":")
+        m[k]=v
+    return m
+
 def main(argv):
     (filename, type, ts)=get_args(argv)
     if type == "trie":
-        t=trie.list_to_trie(ts.split(", "))
+        if is_map(ts):
+            t=trie.map_to_trie(to_map(ts))
+        else:
+            t=trie.list_to_trie(ts.split(", "))
         print trie.trie_to_str(t)
         trie_to_dot(t, filename)
 
