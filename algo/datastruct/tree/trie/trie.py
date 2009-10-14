1194fb5c8291190401ea420a468dbbacfa067ce9
unknown <liuuuxin@.ap.tieto.com>
2009-10-14 14:34:27 +0800

    updated trie.py and trie2dot.py so that we can insert trie from map like: "110:x, 101:y, 001:z"

--- algo/datastruct/tree/trie/trie.py
+++ algo/datastruct/tree/trie/trie.py
@@ -4,26 +4,28 @@ import string
 
 class Trie:
     def __init__(self):
-        self.value = ""
+        self.value = None
         self.children = {}
 
-def trie_insert(t, key):
+def trie_insert(t, key, value = None):
     if t is None:
-        t = Trie();
+        t = Trie()
 
     p = t
     for c in key:
         if c.isdigit():
-            c = eval(c)
+            c = eval(c) # fix a problem in python.dict
         if not c in p.children: 
             p.children[c] = Trie()
         p = p.children[c]
-    p.value = key # this is only for illustration purpose
+    p.value = value
     return t
 
 def trie_to_str(t, prefix=""):
     to_str = lambda x: "%s" %x
     str="("+prefix
+    if not t.value is None:
+        str += ":"+t.value
     for (k,v) in t.children.items():
         str += ", "+trie_to_str(v, prefix+to_str(k))
     str+=")"
@@ -33,6 +35,12 @@ def list_to_trie(l):
     t = None
     for x in l:
         t = trie_insert(t, x)
+    return t
+
+def map_to_trie(m):
+    t = None
+    for k, v in m.items():
+        t = trie_insert(t, k, v)
     return t
 
 class TrieTest:
@@ -54,6 +62,8 @@ class TrieTest:
         print trie_to_str(t)
         t1 = list_to_trie(["011", "11", "0011"])
         print trie_to_str(t1)
+        t2 = map_to_trie({"001":'y', "100":'x', "101":'z'})
+        print trie_to_str(t2)
 
 def main(argv):
     (filename, type, ts)=get_args(argv)
