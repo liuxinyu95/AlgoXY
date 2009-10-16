a54712583b887f949314841ef9a054aa90ebf9e5
liuxinyu.tieto <xinyu.liu@tieto.com>
2009-10-16 11:08:58 +0800

    added integer trie in python implementation.

---
+++ algo/datastruct/tree/trie/src/inttrie.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+import string
+
+class IntTrie:
+    def __init__(self):
+        self.value = None
+        self.left = self.right = None
+
+def trie_insert(t, key, value = None):
+    if t is None:
+        t = IntTrie()
+
+    p = t
+    while key != 0:
+        if key & 1 == 0:
+            if p.left is None:
+                p.left = IntTrie()
+            p = p.left
+        else:
+            if p.right is None:
+                p.right = IntTrie()
+            p = p.right
+        key = key>>1
+    p.value = value
+    return t
+
+def lookup(t, key):
+    while key != 0 and (t is not None):
+        if key & 1 == 0:
+            t = t.left
+        else:
+            t = t.right
+        key = key>>1
+    if t is not None:
+        return t.value
+    else:
+        return None
+
+def trie_to_str(t, prefix=0):
+    to_str = lambda x: "%s" %x
+    str="("+to_str(prefix)
+    if not t.value is None:
+        str += ":"+t.value
+    if not t.left is None:
+        str += ", "+trie_to_str(t.left, (prefix<<1))
+    if not t.right is None:
+        str += ", "+trie_to_str(t.right, (prefix<<1)+1)
+    str+=")"
+    return str
+
+def list_to_trie(l):
+    t = None
+    for x in l:
+        t = trie_insert(t, x)
+    return t
+
+def map_to_trie(m):
+    t = None
+    for k, v in m.items():
+        t = trie_insert(t, k, v)
+    return t
+
+class IntTrieTest:
+    def __init__(self):
+        print "start trie test"
+
+    def run(self):
+        self.test_insert()
+        self.test_lookup()
+
+    def test_insert(self):
+        t = None
+        t = trie_insert(t, 0);
+        t = trie_insert(t, 1);
+        t = trie_insert(t, 4);
+        t = trie_insert(t, 5);
+        print trie_to_str(t)
+        t1 = list_to_trie([1, 4, 5])
+        print trie_to_str(t1)
+        t2 = map_to_trie({4:'y', 1:'x', 5:'z'})
+        print trie_to_str(t2)
+
+    def test_lookup(self):
+        t = map_to_trie({4:'y', 1:'x', 5:'z'})
+        print "look up 4: ", lookup(t, 4)
+        print "look up 0: ", lookup(t, 0)
+
+def main(argv):
+    (filename, type, ts)=get_args(argv)
+    if type == "trie":
+        t=str_to_trie(ts)
+        trie_to_dot(t, filename)
+
+if __name__ == "__main__":
+    IntTrieTest().run()
