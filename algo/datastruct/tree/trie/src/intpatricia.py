52142b665372de4800463a0c835aed155859449c
liuxinyu.tieto <xinyu.liu@tieto.com>
2009-10-23 18:17:03 +0800

    go on writing python version of int patricia

--- algo/datastruct/tree/trie/src/intpatricia.py
+++ algo/datastruct/tree/trie/src/intpatricia.py
@@ -6,25 +6,65 @@ class IntTree:
     def __init__(self, key = None, value = None):
         self.key = key
         self.value = value
-        self.prefix = self.mask = 0
+        self.prefix = self.mask = None
         self.left = self.right = None
+
+    def set_children(l, r):
+        self.left = l
+        self.right = r
+
+    def replace_child(x, y):
+        if self.left == x:
+            self.left = y
+        else:
+            self.right = y
 
     def is_leaf(self):
         return self.left is None and self.right is None
+
+def maskbit(x, mask):
+    pass
+
+def match(key, tree):
+    if tree.is_leaf():
+        return False
+    return maskbit(key, tree.mask) == tree.prefix
+
+def zero(x, mask):
+    pass
+
+def branch(t1, t2):
+    t = IntTree()
+    #need cover cases: t1 is leaf, or t2 is leaf
+    (t.prefix, t.mask) = lcp(t1.prefix, t2.prefix)
+    if zero(t1.prefix, t.mask):
+        t.set_children(t1, t2)
+    else:
+        t.set_children(t2, t1)
+    return t
 
 def insert(t, key, value = None):
     if t is None:
         t = IntTree(key, value)
         return t
 
-    if t.is_leaf():
-        if key == t.key:
-            t.value=value
-        else:
-            t = join(IntTree(key, value), t)
-        return t
-
     node = t
+    parent = None
     while(True):
         if match(key, node):
-            if key = 
+            parent = node
+            if zero(key, node.mask):
+                node = node.left
+            else:
+                node = node.right
+        else:
+            if node.is_leaf() and key == node.key:
+                node.value = value
+            else:
+                new_node = branch(node, IntTree(key, value))
+                if parent is None:
+                    t = new_node
+                else:
+                    parent.replace_child(node, new_node)
+            break
+    return t
