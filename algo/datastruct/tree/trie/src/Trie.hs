87db8196d084a2bfa621dc6cb878f37a04d83427
liuxinyu-home <liuxinyu95@gmail.com>
2009-11-11 13:45:35 +0800

    Haskell version of Trie is drafted, but there are bugs, either in toString func, or in insert func.

--- algo/datastruct/tree/trie/src/Trie.hs
+++ algo/datastruct/tree/trie/src/Trie.hs
@@ -1,18 +1,20 @@
 -- Refer to http://en.wikipedia.org/wiki/Trie
 
 -- definition
-data Trie a = Empty
-            | Trie { value :: Maybe a
+data Trie a = Trie { value :: Maybe a
                    , children :: [(Char, Trie a)]}
+
+empty = Trie Nothing []
+
+isEmpty (Trie Nothing []) = True
+isEmpty _ = False
 
 -- insert
 -- usage: insert t "zoo" "a place where animals are for public to see"
 insert :: Trie a -> String -> a -> Trie a
-insert Empty []     x = Trie (Just x) []
-insert Empty (k:ks) x = Trie Nothing [(k, insert Empty ks x)]
 insert t []     x = Trie (Just x)  (children t)
 insert t (k:ks) x = Trie (value t) (ins (children t) k ks x) where
-    ins [] k ks x = [(k, (insert Empty ks x))]
+    ins [] k ks x = [(k, (insert empty ks x))]
     ins (p:ps) k ks x = if fst p == k 
                         then (k, insert (snd p) ks x):ps
                         else p:(ins ps k ks x)
@@ -20,27 +22,27 @@ insert t (k:ks) x = Trie (value t) (ins (children t) k ks x) where
 -- lookup
 find :: Trie a -> String -> Maybe a
 find t [] = value t
-find Empty _ = Nothing
 find t (k:ks) = case lookup k (children t) of
                   Nothing -> Nothing
                   Just t' -> find t' ks
 
 fromList :: [(String, a)] -> Trie a
-fromList xs = foldl ins Empty xs where
+fromList xs = foldl ins empty xs where
     ins t (k, v) = insert t k v
 
 toString :: (Show a)=> Trie a -> String
 toString t = toStr t "" where
-    toStr :: (Show a) => Trie a -> String -> String
-    toStr Empty prefix = "(" ++ prefix ++ ")"
-    toStr t prefix = "(" ++ prefix ++ ":" ++ (showMaybe (value t)) ++
-                     (concat $ map (\(k, v)->(toStr v prefix++[k])++ ",") (children t)) ++
-                     "), "
+    toStr t prefix = "(" ++ prefix ++ showMaybe (value t) ++ showChildren (children t) prefix ++ "), "
     showMaybe Nothing = ""
-    showMaybe (Just x)  = show x
+    showMaybe (Just x)  = ":" ++ show x
+    showChildren [] _ = ""
+    showChildren (p:ps) prefix = (toStr (snd p) prefix++[(fst p)]) ++", "++ (showChildren ps prefix)
 
-testTrie = "t=" ++ (toString t) 
-    where t = fromList [("a", 1), ("an", 2), ("another", 7), ("boy", 3), ("bool", 4), ("zoo", 3)]
+testTrie = "t=" ++ (toString t)
+    where 
+      t = fromList [("a", 1)]
+      --t = fromList [("a", 1), ("an", 2), ("another", 7), ("boy", 3), ("bool", 4), ("zoo", 3)]
 
 main = do
   putStrLn testTrie
+  putStrLn $ toString ((Trie Nothing [('a', (Trie (Just 1) []))])::(Trie Int))
