b43e0a852b96902e35d958682f5e0285c6d288f3
unknown <liuuuxin@.ap.tieto.com>
2009-11-12 16:24:11 +0800

    latex: lookup in Trie, pseudo code added.

--- algo/datastruct/tree/trie/src/trie.py
+++ algo/datastruct/tree/trie/src/trie.py
@@ -22,7 +22,13 @@ def trie_insert(t, key, value = None):
 def lookup(t, key):
     if t is None:
         return None
-    return None
+
+    p = t
+    for c in key:
+        if not c in p.children:
+            return None
+        p = p.children[c]
+    return p.value
 
 def trie_to_str(t, prefix=""):
     str="("+prefix
@@ -51,6 +57,7 @@ class TrieTest:
 
     def run(self):
         self.test_insert()
+        self.test_lookup()
 
     def test_insert(self):
         t = None
@@ -66,6 +73,12 @@ class TrieTest:
         print trie_to_str(t1)
         t2 = map_to_trie({"001":'y', "100":'x', "101":'z'})
         print trie_to_str(t2)
+
+    def test_lookup(self):
+        t = map_to_trie({"a":1, "an":2, "another":7, "b":1, "bool":4, "bob":3, "home":4})
+        print "find another: ", lookup(t, "another")
+        print "find home: ", lookup(t, "home")
+        print "find the: ", lookup(t, "the")
 
 if __name__ == "__main__":
     TrieTest().run()
