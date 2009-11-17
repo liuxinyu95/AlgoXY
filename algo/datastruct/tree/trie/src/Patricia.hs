c890a87efd2362ff8bf7666b0721135f044f21aa
unknown <liuuuxin@.ap.tieto.com>
2009-11-17 13:31:09 +0800

    Haskell Patricia is OK, but there is a problem of pre-order traverse.
    We need sort children when doing traverse.

--- algo/datastruct/tree/trie/src/Patricia.hs
+++ algo/datastruct/tree/trie/src/Patricia.hs
@@ -3,20 +3,20 @@ type Key = String
 data Patricia a = Patricia { value :: Maybe a
                            , children :: [(Key, Patricia a)]}
 
-empty = Patrcia Nothing []
+empty = Patricia Nothing []
 
 leaf :: a -> Patricia a
 leaf x = Patricia (Just x) []
 
 --insert
 insert :: Patricia a -> Key -> a -> Patricia a
-insert t k x = Patricia (value t) ins (children t) k x where
+insert t k x = Patricia (value t) (ins (children t) k x) where
     ins []     k x = [(k, Patricia (Just x) [])]
     ins (p:ps) k x 
         | (fst p) == k 
-            = (k, Patricia (Just x) children (snd p)):ps --overwrite
+            = (k, Patricia (Just x) (children (snd p))):ps --overwrite
         | match (fst p) k 
-            = (branch k (leaf x) (fst p) (snd p)):ps
+            = (branch k x (fst p) (snd p)):ps
         | otherwise 
             = p:(ins ps k x)
 
@@ -25,15 +25,15 @@ match [] _ = False
 match _ [] = False
 match x y = head x == head y
 
-branch :: Key -> Patricia a -> Key -> Patricia a -> (Key, Patricia a)
-branch k1 t1 k2 t2 
+branch :: Key -> a -> Key -> Patricia a -> (Key, Patricia a)
+branch k1 x k2 t2 
     | k1 == k 
-        -- ex: merge "an" into "another" => ("an", Node t1 ("other", Node t2..))
-        = (k, Patricia (value t1) [(k2', t2)])
+        -- ex: insert "an" into "another" 
+        = (k, Patricia (Just x) [(k2', t2)])
     | k2 == k 
-        -- ex: merge "another" into "an"
-        = (k, Patricia (value t2) [(k1', t1)])
-    | otherwise = (k, Patricia Nothing [(k1', t1), (k2', t2)]) 
+        -- ex: insert "another" into "an"
+        = (k, insert t2 k1' x)
+    | otherwise = (k, Patricia Nothing [(k1', leaf x), (k2', t2)]) 
    where
       k = lcp k1 k2
       k1' = drop (length k) k1
@@ -44,3 +44,24 @@ lcp [] _ = []
 lcp _ [] = []
 lcp (x:xs) (y:ys) = if x==y then x:(lcp xs ys) else []
 
+fromList :: [(Key, a)] -> Patricia a
+fromList xs = foldl ins empty xs where
+    ins t (k, v) = insert t k v
+
+toString :: (Show a)=>Patricia a -> String
+toString t = toStr t "" where
+    toStr t prefix = "(" ++ prefix ++ showMaybe (value t) ++
+                     (concat $ map (\(k, v)->", " ++ toStr v (prefix++k))
+                             (children t))
+                     ++ ")"
+    showMaybe Nothing = ""
+    showMaybe (Just x) = ":" ++ show x
+
+testPatricia = "t1=" ++ (toString t1) ++ "\n" ++
+               "t2=" ++ (toString t2)
+    where
+      t1 = fromList [("a", 1), ("an", 2), ("another", 7), ("boy", 3), ("bool", 4), ("zoo", 3)]
+      t2 = fromList [("zoo", 3), ("bool", 4), ("boy", 3), ("another", 7), ("an", 2), ("a", 1)]
+
+main = do
+  putStrLn testPatricia
