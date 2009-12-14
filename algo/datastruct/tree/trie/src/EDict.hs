eeaa00306751463f49bb2cf07b602e8a9bb9f5f7
unknown <liuuuxin@.ap.tieto.com>
2009-12-14 18:56:35 +0800

    latex: E-dictionary by Trie in Haskell finished.
    Next: E-dictionary by Patricia in Haskell?

--- algo/datastruct/tree/trie/src/EDict.hs
+++ algo/datastruct/tree/trie/src/EDict.hs
@@ -1,6 +1,37 @@
 module Main where
 
-import qualified Trie
+import Trie
+
+-- find all candidates
+findAll:: Trie a -> String -> [(String, a)]
+findAll t [] = 
+    case value t of
+      Nothing -> enum (children t) 
+      Just x  -> ("", x):(enum (children t))
+    where
+      enum [] = []
+      enum (p:ps) = (mapAppend (fst p) (findAll (snd p) [])) ++ (enum ps)
+findAll t (k:ks) = 
+    case lookup k (children t) of
+      Nothing -> []
+      Just t' -> mapAppend k (findAll t' ks)
+
+mapAppend x lst = map (\p->(x:(fst p), (snd p))) lst
+
+-- test
+testFindAll = "t=" ++ (toString t) ++ 
+              "\nlook up a: " ++ (show $ take 5 $findAll t "a") ++
+              "\nlook up ab: " ++ (show $ take 5 $findAll t "ab")
+    where 
+      t = fromList [("a", "the first letter of English"), 
+                    ("an", "used instead of 'a' when the following word begins with a vowel sound"), 
+                    ("another", "one more person or thing or an extra amount"), 
+                    ("abandon", "to leave a place, thing or person forever"),
+                    ("about", "on the subject of; connected with"),
+                    ("adam", "a character in the Bible who was the first man made by God"),
+                    ("boy", "a male child or, more generally, a male of any age"), 
+                    ("bodyl", "the whole physical structure that forms a person or animal"), 
+                    ("zoo", "an area in which animals, especially wild animals, are kept so that people can go and look at them, or study them")]
 
 main = do
-    putStrLn Trie.testTrie
+    putStrLn testFindAll
