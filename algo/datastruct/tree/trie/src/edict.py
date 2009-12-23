3935c3fc661a7d5c348b41ef2df45d517fe9c6d9
unknown <liuuuxin@.ap.tieto.com>
2009-12-23 10:55:38 +0800

    Python version of T9 search with Trie is OK.
    Next is to write T9 search with Patricia

--- algo/datastruct/tree/trie/src/edict.py
+++ algo/datastruct/tree/trie/src/edict.py
@@ -45,9 +45,27 @@ def patricia_lookup(t, key, n):
                 break
         if not match:
             return None
+
+T9MAP={'2':"abc", '3':"def", '4':"ghi", '5':"jkl", \
+       '6':"mno", '7':"pqrs", '8':"tuv", '9':"wxyz"}
                 
 def trie_lookup_t9(t, key):
-    pass
+    if t is None or key == "":
+        return None
+    q = [("", key, t)]
+    res = []
+    while len(q)>0:
+        (prefix, k, t) = q.pop(0)
+        i=k[0]
+        if not i in T9MAP:
+            return None #invalid input
+        for c in T9MAP[i]:
+            if c in t.children:
+                if k[1:]=="":
+                    res.append((prefix+c, t.children[c].value))
+                else:
+                    q.append((prefix+c, k[1:], t.children[c]))
+    return res
 
 def patricia_lookup_t9(t, key):
     pass
@@ -66,6 +84,9 @@ class LookupTest:
            "zoo":"an area in which animals, especially wild animals, are kept so that people can go and look at them, or study them"}
         self.tt = trie.map_to_trie(dict)
         self.tp = patricia.map_to_patricia(dict)
+        t9dict = ["home", "good", "gone", "hood", "a", "another", "an"]
+        self.t9t = trie.list_to_trie(t9dict)
+        self.t9p = patricia.list_to_patricia(t9dict)
 
     def run(self):
         self.test_trie_lookup()
@@ -84,7 +105,12 @@ class LookupTest:
         print "search ab ", patricia_lookup(self.tp, "ab", 5)
 
     def test_trie_t9(self):
-        pass
+        print "test t9 lookup in Trie"
+        print "search 4 ", trie_lookup_t9(self.t9t, "4")
+        print "search 46 ", trie_lookup_t9(self.t9t, "46")
+        print "search 4663 ", trie_lookup_t9(self.t9t, "4663")
+        print "search 2 ", trie_lookup_t9(self.t9t, "2")
+        print "search 22 ", trie_lookup_t9(self.t9t, "22")
 
     def test_patricia_t9(self):
         pass
