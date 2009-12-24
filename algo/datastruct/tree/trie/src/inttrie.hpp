cf7106aad563c05b226dbb960f0697158310743c
unknown <liuuuxin@.ap.tieto.com>
2009-12-24 14:50:38 +0800

    CPP version of Int Trie insertion is OK.

---
+++ algo/datastruct/tree/trie/src/inttrie.hpp
@@ -0,0 +1,110 @@
+#ifndef _INT_TRIE
+#define _INT_TRIE
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <list>
+#include <iterator>
+
+template<class T>
+struct IntTrie{
+  IntTrie():value(), left(0), right(0){}
+  ~IntTrie(){
+    delete left;
+    delete right;
+  }
+  T value;
+  IntTrie* left;
+  IntTrie* right;
+};
+
+template<class T>
+IntTrie<T>* insert(IntTrie<T>* t, int key, T value=T()){
+  if(!t)
+    t = new IntTrie<T>();
+
+  IntTrie<T>* p = t;
+  while(key){
+    if( (key&0x1) == 0){
+      if(!p->left) p->left = new IntTrie<T>();
+      p = p->left;
+    }
+    else{
+      if(!p->right) p->right = new IntTrie<T>();
+      p = p->right;
+    }
+    key>>=1;
+  }
+  p->value = value;
+  return t;
+}
+
+template<class T>
+std::string trie_to_str(IntTrie<T>* t, int prefix=0, int depth=0){
+  std::stringstream s;
+  s<<"("<<prefix;
+  if(t->value!=T())
+    s<<":"<<t->value;
+  if(t->left)
+    s<<", "<<trie_to_str(t->left, prefix, depth+1);
+  if(t->right)
+    s<<", "<<trie_to_str(t->right, (1<<depth)+prefix, depth+1);
+  s<<")";
+  return s.str();
+}
+
+template<class T, class Iterator>
+IntTrie<T>* list_to_trie(Iterator first, Iterator last){
+  IntTrie<T>* t(0);
+  for(;first!=last; ++first)
+    t = insert(t, *first);
+  return t;
+}
+
+template<class T, class Iterator>
+IntTrie<T>* map_to_trie(Iterator first, Iterator last){
+  IntTrie<T>* t(0);
+  for(;first!=last; ++first)
+    t = insert(t, first->first, first->second);
+  return t;
+}
+
+class IntTrieTest{
+public:
+  IntTrieTest():ti(0), tc(0){}
+
+  ~IntTrieTest(){
+    delete ti;
+    delete tc;
+  }
+
+  void run(){
+    test_trie_insert();
+  }
+private:
+  void test_trie_insert(){
+    const int lst[] = {1, 4, 5};
+    std::list<int> l(lst, lst+sizeof(lst)/sizeof(int));
+
+    ti = list_to_trie<int, std::list<int>::iterator>(l.begin(), l.end());
+    std::copy(l.begin(), l.end(), 
+              std::ostream_iterator<int>(std::cout, ", "));
+    std::cout<<"==>"<<trie_to_str(ti)<<"\n";
+    
+    typedef std::list<std::pair<int, char> > Dict;
+    const int  keys[] = {4, 1, 5, 9};
+    const char vals[] = "bacd";
+    Dict m;
+    for(int i=0; i<sizeof(keys)/sizeof(int); ++i)
+      m.push_back(std::make_pair(keys[i], vals[i]));
+    tc = map_to_trie<char, Dict::iterator>(m.begin(), m.end());
+    std::copy(keys, keys+sizeof(keys)/sizeof(int),
+              std::ostream_iterator<int>(std::cout, ", "));
+    std::cout<<"==>"<<trie_to_str(tc);
+  }
+
+  IntTrie<int>* ti;
+  IntTrie<char>* tc;
+};
+
+#endif //_INT_TRIE
---
+++ algo/datastruct/tree/trie/src/test.cpp
@@ -0,0 +1,5 @@
+#include "inttrie.hpp"
+
+int main(int, char**){
+  IntTrieTest().run();
+}
