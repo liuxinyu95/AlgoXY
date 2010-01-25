3fd3d949399c325bbc9d8c4169d4141c5a9e9394
unknown <liuuuxin@.ap.tieto.com>
2010-01-25 10:17:58 +0800

    Add GPL license header to all source files under trie folder.
    Next is to develop GPL adder scripts, and add GPL header to bst and rbtree.

--- algo/datastruct/tree/trie/src/EDict.hs
+++ algo/datastruct/tree/trie/src/EDict.hs
@@ -1,3 +1,21 @@
+{-
+    Edict.hs, E-dictionary and T9 input method with Trie and Patricia.
+    Copyright (C) 2010, Liu Xinyu (liuxinyu95@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 module Main where
 
 import qualified Data.List
