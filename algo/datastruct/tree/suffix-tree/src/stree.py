c2e5c26b6db4a7c76490a8783087a1c9d5938d7f
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-03-08 14:44:29 +0800

    go on writing about suffix tree python program.

--- algo/datastruct/tree/suffix-tree/src/stree.py
+++ algo/datastruct/tree/suffix-tree/src/stree.py
@@ -18,11 +18,69 @@
 
 import string
 
-class STree:
-    def __init__(self, suffix = None):
+class Node:
+    def __init__(self, suffix=None):
         self.children = {}
         self.suffix = suffix
 
+class STree:
+    def __init__(self, s):
+        self.str = s
+        self.infinity = len(s)+1000
+        self.root = Node()
+
+# In order to improve the efficiency by operation on-line
+# There is only one copy of the string. All sub strings are
+# represented as reference pair: 
+#   w = (k, p)
+# where: w = str[k:p+1], +1 is because of Python's specific
+# problem
+def substr(str, left, right):
+    return str[left:right+1]
+
+def infinity(str):
+    return len(str)+1000
+
+# left: k in Ukkonen '95
+# node: s in Ukkonen '95
 def suffix_tree(str):
-    t = STree()
-    for 
+    t = STree(str)
+    node = t.root # init active point is root
+    left = 0
+    for i in range(len(str)):
+        (node, left) = update(t, node, (left, i))
+        (node, left) = canonize(t, node, (left, i))
+    return t
+
+# prev: oldr in Ukkonen '95
+# p:    r in Ukkonen '95
+def update(t, node, str_ref):
+    (left, i) = str_ref 
+    # (node, (left, right-1)), canonical ref pair for the active point
+    prev = Node() # dummy init value
+    (is_end_point, p) = branch(t, node, (left, i-1), t.str[i])
+    while not is_end_point:
+        p.children[t.str[i]]=((i, t.infinity), Node())
+        prev.suffix = p
+        prev = p
+        (node, k) = canonize(t, node.suffix, (k, i-1))
+        (is_end_point, p) = branch(t, node, (k, i-1), t.str[i])
+    if prev != t.root:
+        prev.suffix = node
+    return (node, k)
+
+# branch: test-and-split in Ukkonen '95
+#  test if (node, str_ref) is the end point
+#  if not, then branch out a new node
+def branch(t, node, str_ref, c):
+    (left, right) = str_ref
+    if left <= right:
+        ((left1, right1), node1) = node.children[t.str[left]]
+    else:
+        return (c in node.children, node)
+            
+def canonize(node, str_ref):
+    if node is None:
+        pass
+    (left, right) = str_ref
+    if
