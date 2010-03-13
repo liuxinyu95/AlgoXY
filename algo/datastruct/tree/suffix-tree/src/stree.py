335367b76fe1d42407097ccab2714f6ca99e4d89
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-03-13 18:37:52 +0800

    updated.

--- algo/datastruct/tree/suffix-tree/src/stree.py
+++ algo/datastruct/tree/suffix-tree/src/stree.py
@@ -20,7 +20,7 @@ import string
 
 class Node:
     def __init__(self, suffix=None):
-        self.children = {}
+        self.children = {} # 'c':(word, Node), where word = (l, r)
         self.suffix = suffix
 
 class STree:
@@ -38,9 +38,6 @@ class STree:
 def substr(str, left, right):
     return str[left:right+1]
 
-def infinity(str):
-    return len(str)+1000
-
 # left: k in Ukkonen '95
 # node: s in Ukkonen '95
 def suffix_tree(str):
@@ -52,35 +49,63 @@ def suffix_tree(str):
         (node, left) = canonize(t, node, (left, i))
     return t
 
+# Main func: STree(Str[i-1]) ==> STree(Str[i])
 # prev: oldr in Ukkonen '95
 # p:    r in Ukkonen '95
 def update(t, node, str_ref):
     (left, i) = str_ref 
+    c = t.str[i] # current char
     # (node, (left, right-1)), canonical ref pair for the active point
     prev = Node() # dummy init value
-    (is_end_point, p) = branch(t, node, (left, i-1), t.str[i])
-    while not is_end_point:
-        p.children[t.str[i]]=((i, t.infinity), Node())
+    (finish, p) = branch(t, node, (left, i-1), c)
+    while not finish:
+        p.children[c]=((i, t.infinity), Node())
         prev.suffix = p
         prev = p
-        (node, k) = canonize(t, node.suffix, (k, i-1))
-        (is_end_point, p) = branch(t, node, (k, i-1), t.str[i])
+        if node.suffix is None:
+            break
+        (node, left) = canonize(t, node.suffix, (left, i-1))
+        (finish, p) = branch(t, node, (left, i-1), c)
     if prev != t.root:
         prev.suffix = node
-    return (node, k)
+    return (node, left)
 
-# branch: test-and-split in Ukkonen '95
-#  test if (node, str_ref) is the end point
+# branch: 
+#  test if (node, str_ref) has c-transition already
 #  if not, then branch out a new node
+# case 1:
+#   (root, empty), c ==> root.children[c] is None?
+# case 2:
+#   (root, (0, i-1)), c ==> add c to top, always return true, root
+# case 3:
+#
 def branch(t, node, str_ref, c):
-    (left, right) = str_ref
-    if left <= right:
-        ((left1, right1), node1) = node.children[t.str[left]]
+    (l, r) = str_ref
+    if l <= r:
+        ((l1, r1), node1) = node.children[t.str[l]]
+        if t.str[l1+(r-l+1)]==c:
+            return (True, node)
+        else:
+            # node--->branch_node--->node1
+            branch_node = Node()
+            pos = l1+r-l
+            node.children[t.str[l1]]=((l1, pos), branch_node)
+            branch_node.children[t.str[pos+1]] = ((pos+1, r1), node1)
+            return (False, branch_node)
     else:
         return (c in node.children, node)
-            
-def canonize(node, str_ref):
-    if node is None:
-        pass
-    (left, right) = str_ref
-    if
+
+# node[c]--->(l, r), _ 
+# node[c]--->((l', r'), node')--->...-->((l'', r''), node'')--->((x, inf), leaf)
+# where _: it may not be a node, but some implicity position
+# find the closet node and left, so that they point to same position _
+def canonize(t, node, str_ref):
+    (l, r) = str_ref 
+    while l<=r: # str_ref is not empty
+        ((l1, r1), child) = node.children[t.str[l]] # node--(l', r')-->child
+        if r-l >= r1-l1: #node--(l',r')-->child--->...
+            l += r1-l1+1 # remove |(l',r')| chars from (l, r)
+            node = child 
+        else:
+            break
+    return (node, l)
