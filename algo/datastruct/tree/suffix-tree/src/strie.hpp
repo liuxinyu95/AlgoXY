e2035bd06d944f717081c4d3f0ad7755965e95e2
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-04-16 18:16:02 +0800

    added c++ version of suffix trie, will abstract common part also from suffix tree.

---
+++ algo/datastruct/tree/suffix-tree/src/strie.hpp
@@ -0,0 +1,33 @@
+//     strie.hpp, Suffix Trie online construction algorithm.
+//     Copyright (C) 2010, Liu Xinyu (liuxinyu95@gmail.com)
+
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _STRIE_
+#define _STRIE_
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <iterator>
+#include <sstream>
+#include <list>
+#include <algorithm>
+
+struct STrie{
+
+};
+
+
+#endif //_STRIE_
