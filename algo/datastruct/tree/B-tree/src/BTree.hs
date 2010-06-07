9605e1327a043e445a9205fe8a9bd995bb826634
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-06-07 18:00:40 +0800

    Haskell btree insert is drafted, may contain bugs.

--- algo/datastruct/tree/B-tree/src/BTree.hs
+++ algo/datastruct/tree/B-tree/src/BTree.hs
@@ -26,35 +26,34 @@ data BTree a = Node{ keys :: [a]
 
 empty deg = Node [] [] deg
 
--- use insert - then -balance methods, which is similar to
--- Okasaki's red-black tree algorithm
+full::BTree a -> Bool
+full tr = (length $ keys tr) >= 2*(degree tr)-1
+
 insert :: (Ord a)=> BTree a -> a -> BTree a
 insert tr x = fix $ ins tr where
-    ins (Node ks [] t) = split (Node ([a|a<-ks, a<x]++[x]++[b|b<-ks, b>x]) [] t)
-    ins (Node (k:ks) (cl:cr:cs) t)
-        | x < k = merge (ins cl) (Node (k:ks) (cr:cs) t)
-        | otherwise = merge (Node [k] [cl] t) (ins (Node ks (cr:cs) t))
-    ins (Node [] [c] t) = Node [] [ins c] t
+    ins (Node ks [] t) = Node ([a|a<-ks, a<x]++[x]++[b|b<-ks, b>x]) [] t
+    ins (Node (k:ks) (c:cs) t)
+        | x < k = merge (ins $ Node [] [c] t) (Node (k:ks) cs t)
+        | otherwise = merge (Node [k] [c] t) (ins $ Node ks cs t)
+    ins (Node [] [c] t) = let c' = ins c in if full c' then split c' else Node [] [c'] t --fix $ Node [] [ins c] t
 
 fix :: BTree a -> BTree a
-fix (Node [] [tr] _) = tr
-fix tr = tr
+fix (Node [] [tr] _) = if full tr then split tr else tr -- Shrink height
+fix tr = if full tr then split tr else tr
 
 merge :: BTree a -> BTree a -> BTree a
-merge (Node ks1 cs1 t) (Node ks2 cs2 _) = split (Node (ks1++ks2) (cs1++cs2) t)
+merge (Node ks1 cs1 t) (Node ks2 cs2 _) = fix $ Node (ks1++ks2) (cs1++cs2) t
 
 -- take n [] == drop n[] == []
 split :: BTree a -> BTree a
-split (Node ks cs t) 
-    |length ks < 2*t-1 = Node []  [Node ks cs t] t
-    |otherwise = Node [k] [c1, c2] t where
-        c1 = Node (take (t-1) ks) (take t cs) t
-        c2 = Node (drop t ks) (drop t cs) t
-        k  = head (drop (t-1) ks)
+split (Node ks cs t) = Node [k] [c1, c2] t where
+    c1 = Node (take (t-1) ks) (take t cs) t
+    c2 = Node (drop t ks) (drop t cs) t
+    k  = head (drop (t-1) ks)
 
 toString :: (Show a)=>BTree a -> String
-toString (Node ks [] _) = "("++(intersperse ',' (show ks))++")"
-tostring tr = "("++(toStr (keys tr) (children tr))++")" where
+toString (Node ks [] _) = "("++(concat $ intersperse "," (map show ks))++")"
+toString tr = "("++(toStr (keys tr) (children tr))++")" where
     toStr (k:ks) (c:cs) = (toString c)++", "++(show k)++", "++(toStr ks cs)
     toStr [] [c] = toString c
 
