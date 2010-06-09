dfc0c80d62e7d0059257bd2b4c207f9db75ff987
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-06-09 18:22:34 +0800

    adding delete B-tree function in Haskell, not finished.

--- algo/datastruct/tree/B-tree/src/BTree.hs
+++ algo/datastruct/tree/B-tree/src/BTree.hs
@@ -18,7 +18,7 @@
 
 module BTree where
 
-import Data.List (intersperse, partition)
+import qualified Data.List as L
 
 data BTree a = Node{ keys :: [a]
                    , children :: [BTree a]
@@ -29,17 +29,18 @@ empty deg = Node [] [] deg
 full::BTree a -> Bool
 full tr = (length $ keys tr) >= 2*(degree tr)-1
 
+low::BTree a -> Bool
+low tr = (length $ keys tr) < (degree tr)-1
+
 -- take n [] == drop n[] == []
 insert :: (Ord a)=> BTree a -> a -> BTree a
-insert (Node ks [] t) x = fix $ Node ([a|a<-ks, a<x]++[x]++[b|b<-ks, b>x]) [] t
+insert (Node ks [] t) x = fix $ Node (L.insert x ks) [] t
 insert (Node ks cs t) x = fix $ merge left (insert c x) right
     where
       left  = (ks' , cs' )
       right = (ks'', cs'')
-      (ks', ks'') = partition (< x) ks
-      cs' = take (length ks') cs
-      cs''= drop (length cs' +1) cs
-      c   = head $ drop (length cs') cs
+      (ks', ks'') = L.partition (< x) ks
+      (cs', (c:cs'')) = L.splitAt (length ks') cs
 
 fix :: BTree a -> BTree a
 --fix (Node [] [tr] _) = if full tr then split tr else tr -- Shrink height
@@ -53,10 +54,33 @@ split (Node ks cs t) = Node [k] [c1, c2] t where
 
 merge :: ([a], [BTree a]) -> BTree a -> ([a], [BTree a]) -> BTree a
 merge (ks', cs') (Node [k] cs t) (ks'', cs'') = Node (ks'++[k]++ks'') (cs'++cs++cs'') t
-merge (ks', cs') c (ks'', cs'') = Node (ks'++ks'') (cs'++[c]++cs'') (degree c)
+merge (ks', cs') c (ks'', cs'') 
+    | low c = borrow (ks', cs') c (ks'', cs'')
+    | otherwise = Node (ks'++ks'') (cs'++[c]++cs'') (degree c)
+
+borrow :: ([a], [BTree a]) -> BTree a -> ([a], [BTree a]) -> BTree a
+borrow ([], []) (Node ks cs t) ((k':ks'), (c':cs')) = 
+    Node ks' ((Node (ks++[k']) (cs++[c']) t):cs') t
+borrow (ks'@(k':_), cs'@(c':_)) (Node ks cs t) (ks'', cs'') = 
+    Node ((init ks')++ks'') 
+         ((init cs')++[Node ((last ks'):ks) ((last cs'):cs) t]++cs'') t
+borrow _ c _ = c
+
+delete :: (Ord a)=> BTree a -> a -> BTree a
+delete (Node ks [] t) x = Node (L.delete x ks) [] t
+delete (Node ks cs t) x = merge left (delete c x) right
+    where
+      (ks', ks'') = L.partition (<x) ks
+      (cs', cs'') = L.splitAt (length ks') cs
+      left = (ks', cs')
+      right = if head ks'' == x then (tail ks'', drop 2 cs'')
+              else (ks'', tail cs'')
+      c = if head ks'' == x 
+          then fix $ Node ((keys c1)++(keys c2)) ((children c1)++(children c2)) t
+          else head cs''
 
 toString :: (Show a)=>BTree a -> String
-toString (Node ks [] _) = "("++(concat $ intersperse "," (map show ks))++")"
+toString (Node ks [] _) = "("++(L.intercalate "," (map show ks))++")"
 toString tr = "("++(toStr (keys tr) (children tr))++")" where
     toStr (k:ks) (c:cs) = (toString c)++", "++(show k)++", "++(toStr ks cs)
     toStr [] [c] = toString c
@@ -67,4 +91,6 @@ listToBTree lst t = foldl insert (empty t) lst
 --test
 testInsert = (toString $ listToBTree "GMPXACDEJKNORSTUVYZ" 3) ++"\n"++
              (toString $ listToBTree "GMPXACDEJKNORSTUVYZ" 2)
+
+--testDelete = foldl delete (listToBTree "GMPXACDEJKNORSTUVYZ" 3) lst
        
