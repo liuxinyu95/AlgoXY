886b8bda523b6d72b4ab6c56bb1cb79ef9f71293
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-07-16 17:39:38 +0800

    btree.hpp: passed compile, however, crashed.

--- algo/datastruct/tree/B-tree/src/btree.hpp
+++ algo/datastruct/tree/B-tree/src/btree.hpp
@@ -20,6 +20,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <sstream>    //std::ostringstream
 #include <numeric>    //std::accumulate
 #include <functional> //std::ptr_fun
 #include <vector>     //random access needed.
@@ -34,7 +35,7 @@ struct BTree{
   BTree(){}
 
   ~BTree(){
-    for(Children::iterator it=children.begin();
+    for(typename Children::iterator it=children.begin();
         it!=children.end(); ++it)
       delete (*it);
   }
@@ -46,13 +47,13 @@ struct BTree{
   void split_child(int i){
     BTree<K, t>* x = children[i];
     BTree<K, t>* y = new BTree<K, t>();
-    keys.insert(keys.begin()+i, x.keys[t-1]);
+    keys.insert(keys.begin()+i, x->keys[t-1]);
     children.insert(children.begin()+i+1, y);
-    y->keys = Keys(x.keys.begin()+t, x.keys.end());
-    x->keys = Keys(x.keys.begin(), x.keys.begin()+t-1);
+    y->keys = Keys(x->keys.begin()+t, x->keys.end());
+    x->keys = Keys(x->keys.begin(), x->keys.begin()+t-1);
     if(!y->leaf()){
-      y->children = Children(x.children.begin()+t, x.children.end());
-      x->children = Children(x.children.begin(), x.children.begin()+t);
+      y->children = Children(x->children.begin()+t, x->children.end());
+      x->children = Children(x->children.begin(), x->children.begin()+t);
     }
   }
 
@@ -79,8 +80,8 @@ BTree<K, t>* insert(BTree<K, t>* tr, K key){
 }
 
 template<class Coll>
-void orderred_insert(Coll& coll, Coll::value_type x){
-  Coll::iterator it = coll.begin();
+void orderred_insert(Coll& coll, typename Coll::value_type x){
+  typename Coll::iterator it = coll.begin();
   while(it != coll.end() && *it < x)
     ++it;
   coll.insert(it, x);
@@ -88,15 +89,15 @@ void orderred_insert(Coll& coll, Coll::value_type x){
 
 template<class K, int t>
 BTree<K, t>* insert_nonfull(BTree<K, t>* tr, K key){
-  typedef BTree<K, t>::Keys Keys;
-  typedef BTree<K, t>::Children Children;
+  typedef typename BTree<K, t>::Keys Keys;
+  typedef typename BTree<K, t>::Children Children;
 
   BTree<K, t>* root(tr);
   while(!tr->leaf()){
-    int i=0;
+    unsigned int i=0;
     while(i < tr->keys.size() && tr->keys[i] < key)
       ++i;
-    if(tr->children[i].full()){
+    if(tr->children[i]->full()){
       tr->split_child(i);
       if(key > tr->keys[i])
         ++i;
@@ -107,27 +108,68 @@ BTree<K, t>* insert_nonfull(BTree<K, t>* tr, K key){
   return root;
 }
 
+template<class T>
+T* insert_key(T* t, typename T::key_type x){
+  return insert(t, x);
+}
+
 template<class Iterator, class T>
 T* list_to_btree(Iterator first, Iterator last, T* t){
   return std::accumulate(first, last, t,
-                         std::ptr_fun(insert<T, typename T::key_type>));
+                         std::ptr_fun(insert_key<T>));
 }
 
 //generic auxiliary functions
 template<class T>
 std::string to_str(T x){
-  ostringstream s;
+  std::ostringstream s;
   s<<x;
   return s.str();
 }
 
-//concat(intersperse(", ", map(ptr_fun(to_str), coll)))
-
 template<class T>
 std::string btree_to_str(T* tr){
+  typename T::Keys::iterator k;
+  typename T::Children::iterator c;
+
   std::ostringstream s;
   s<<"(";
   if(tr->leaf()){
-
+    k=tr->keys.begin();
+    s<<*k++;
+    for(; k!=tr->keys.end(); ++k)
+      s<<", "<<*k;
   }
+  else{
+    for(k=tr->keys.begin(), c=tr->children.begin();
+        k!=tr->keys.end(); ++k, ++c)
+      s<<btree_to_str(*c)<<", "<<*k<<", ";
+    s<<btree_to_str(*c);
+  }
+  s<<")";
+  return s.str();
 }
+
+class BTreeTest{
+public:
+  BTreeTest(){
+    std::cout<<"B-tree testing\n";
+  }
+
+  void run(){
+    test_insert();
+  }
+private:
+  void test_insert(){
+    const char* ss[] = {"G", "M", "P", "X", "A", "C", "D", "E", "J", "K", \
+                        "N", "O", "R", "S", "T", "U", "V", "Y", "Z"};
+    BTree<std::string, 2>* tr234=list_to_btree(ss, ss+sizeof(ss)/sizeof(char*),
+                                               new BTree<std::string, 2>);
+    std::cout<<"2-3-4 tree of ";
+    std::copy(ss, ss+sizeof(ss)/sizeof(char*), 
+              std::ostream_iterator<std::string>(std::cout, ", "));
+    std::cout<<"\n"<<btree_to_str(tr234)<<"\n";
+  }
+};
+
+#endif //_B_TREE_
