32aabb4f2a88ce0c17cd4850192e2e81cb6f284e
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-07-22 17:08:44 +0800

    update C++ insert part into latex manuscript.

--- algo/datastruct/tree/B-tree/create-img.sh
+++ algo/datastruct/tree/B-tree/create-img.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
 src/bt2dot img/btree-example.dot "(((A, B), C, (D, E, F), G, (H, I, J, K)), M, ((N, O), P, (Q, R, S), T, (U, V), W, (X, Y, Z)))"
+src/bt2dot img/btree-insert-2-3-4.dot "(((A), C, (D)), E, ((G, J, K), M, (N, O)), P, ((R), S, (T), U, (V), X, (Y, Z)))"
+src/bt2dot img/btree-insert-3.dot "((A, C), D, (E, G, J, K), M, (N, O), P, (R, S), T, (U, V, X, Y, Z))"
