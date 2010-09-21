ae14e1ddd3e09923ccd0e07a714a7b640067cee5
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-09-21 18:12:16 +0800

    start writing pure imperative heap in python.

--- algo/datastruct/heap/binary-heap/src/bheap.py
+++ algo/datastruct/heap/binary-heap/src/bheap.py
@@ -15,3 +15,55 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# auxiliary functions 
+
+def parent(i):
+    return i//2
+
+def left(i):
+    return  2*i
+
+def right(i):
+    return 2*i+1
+
+# max-heapify by default
+def heapify(x, i):
+    heapify_by(lambda a, b: a<b, x, i) 
+
+def heapify_by(less_p, x, i):
+    n = len(x)
+    while True:
+        l = left(i)
+        r = right(i)
+        largest = i
+        if l < n and less_p(x[i], x[l]):
+            largest = l
+        if r < n and less_p(x[largest], x[r]):
+            largest = r
+        if largest != i:
+            (x[i], x[largest])=(x[largest], x[i])
+            i  = largest
+        else:
+            break
+
+# build max heap by default
+def build_heap(x):
+    n = len(x)
+    for i in reversed(range(n//2)):
+        heapify(x, i)
+
+class TestHeap:
+    def __init__(self):
+        print "Implicit binary heap by array testing"
+
+    def run(self):
+        self.test_heapify()
+
+    def test_heapify(self):
+        l = [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
+        heapify(l, 2)
+        print l
+
+if __name__ == "__main__":
+    TestHeap().run()
