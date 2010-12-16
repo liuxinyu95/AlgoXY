645265bfa00a7b64887951a2ddc27400f36c37b5
liuxinyu.tieto <xinyu.liu@tieto.com>
2010-12-16 17:37:12 +0800

    Scheme/Lisp splay heap finished.

--- algo/datastruct/heap/binary-heap/src/skewheap.scm
+++ algo/datastruct/heap/binary-heap/src/skewheap.scm
@@ -16,6 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 |#
 
+;; include generic functions
+(load "genheap.scm")
+
 ;; for pretty print purpose, the skew tree is arranged as
 ;; (left element right)
 
