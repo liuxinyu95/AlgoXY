54d13b53d2adb0e99445bd0b67739c7c76877823
liuxinyu.tieto <xinyu.liu@tieto.com>
2011-02-23 15:14:08 +0800

    updated.

--- algo/others/preface/src/minfree.c
+++ algo/others/preface/src/minfree.c
@@ -20,7 +20,19 @@
 #include "randlist.h"
 #include "measure.h"
 
-#define N 10000000
+#define N 100000 // 0.1 million
+#define WORD_LENGTH sizeof(int)
+
+void setbit(unsigned int* bits, unsigned int i){
+  bits[i / WORD_LENGTH] |= 1<<(i % WORD_LENGTH);
+}
+
+int testbit(unsigned int* bits, unsigned int i){
+  return bits[i/WORD_LENGTH] & (1<<(i % WORD_LENGTH));
+}
+
+unsigned int bits[N/WORD_LENGTH+1];
+
 int ids[N];
 
 void init_ids(){
@@ -29,6 +41,7 @@ void init_ids(){
     ids[i] = i;
 }
 
+// min-free, brute force version
 int brute_force(int* xs, int n){
   int i, j;
   for(i=0; ; ++i){
@@ -42,26 +55,81 @@ int brute_force(int* xs, int n){
   }
 }
 
+// min-free, flags version
+int flag_map(int*xs, int n){
+  int* flags = (int*)malloc(sizeof(int)*(n+1));
+  int i;
+  for(i=0; i<=n; ++i)
+    flags[i] = 0;
+  for(i=0; i<n; ++i)
+    if(xs[i]<n)
+      flags[xs[i]]=1;
+  for(i=0; i<=n; ++i)
+    if(!flags[i]){
+      free(flags);
+      return i;
+    }
+  return n; //shouldn't be here
+}
+
+// min-free, bitmap version
+int bitmap_min_free(int* xs, int n){
+  int i, len = N/WORD_LENGTH+1;
+  for(i=0; i<len; ++i)
+    bits[i]=0;
+  for(i=0; i<n; ++i)
+    if(xs[i]<n)
+      setbit(bits, xs[i]);
+  for(i=0; i<=n; ++i)
+    if(!testbit(bits, i))
+      return i;
+  return n; //shouldn't be here
+}
+
 int fire_brute_force(int x, int y){
   return brute_force((int*)x, y);
 }
 
-double measure_brute_force(){
+int fire_flags(int x, int y){
+  return flag_map((int*)x, y);
+}
+
+int fire_bits(int x, int y){
+  return bitmap_min_free((int*)x, y);
+}
+
+double measure_min_free(Fun f){
   double t, tm = 0.0;
   int i;
-  printf("start measure brute force method...\n");
+  printf("start measure min_free...\n");
   for(i = 0; i < 100; ++i){
     shuffle(ids, N);
-    t = measure(fire_brute_force, (int)ids, N-1);
-    //printf("one search in %f[s]\n", t);
+    t = measure(f, (int)ids, N-1);
+    //printf("%f[s] elapsed\n", t);
     tm = tm + t;
   }
   return tm / 100.00;
 }
 
 int main(){
-  printf("average time of brute-force method: %f[s]\n",
-	 measure_brute_force());
-  //average time of brute-force method: 0.044870[s]
+  init_ids();
+  /* printf("average time of brute-force method: %f[s]\n", */
+  /* 	 measure_min_free(fire_brute_force)); */
+
+  printf("average time of flags method: %f[s]\n",
+   	 measure_min_free(fire_flags));
+
+  printf("average time of bitmap method: %f[s]\n",
+	 measure_min_free(fire_bits));
+
   return 0;
 }
+
+/*
+ * Some performance data
+ *
+ * n = 100,000   0.1 million
+ * brute force: 5.4 [s]
+ * flags: average time of flags method: 0.001860[s]
+ * bitmap: 
+ */
