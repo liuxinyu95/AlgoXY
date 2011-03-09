ea341e1b864f6d008a49c8b535977d2b57de690f
liuxinyu.tieto <xinyu.liu@tieto.com>
2011-03-09 16:49:38 +0800

    started write q235 final solution.

--- algo/datastruct/elementary/problems/q235/q235.py
+++ algo/datastruct/elementary/problems/q235/q235.py
@@ -19,5 +19,27 @@ def uniq_insert(q, x):
     q.insert(i, x)
     return q
         
+def get1(k):
+    if k == 1:
+        return 1
+    (q2, q3, q5) = ([2], [3], [5])
+    while k > 1:
+        x = min(q2[0], q3[0], q5[0])
+        if x == q2[0]:
+            q2.pop(0)
+            q2.append(x*2)
+            q3.append(x*3)
+            q5.append(x*5)
+        elif x == q3[0]:
+            q3.pop(0)
+            q3.append(x*3)
+            q5.append(x*5)
+        else:
+            q5.pop(0)
+            q5.append(x*5)
+        k = k - 1
+    return x
+
 if __name__ == "__main__":
     print get(1500)
+    print get1(1500)
