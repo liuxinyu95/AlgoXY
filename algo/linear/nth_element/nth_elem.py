b665d5cc02ee319089b64f3da9b1761e2ec101b8
liuxinyu.tieto <xinyu.liu@tieto.com>
2011-03-17 14:12:07 +0800

    added GPL license

--- algo/linear/nth_element/nth_elem.py
+++ algo/linear/nth_element/nth_elem.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 
+# nth_elem.py
+# Copyright (C) 2011 Liu Xinyu (liuxinyu95@gmail.com)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+
 import random
 
 def partition_with(xs, l, u, x):
