0de97c6b9690c434a90d4cd155acc47a7bf06a2c
liuxinyu.tieto <xinyu.liu@tieto.com>
2011-04-18 18:15:26 +0800

    added figures to illustrate deletion.

--- algo/datastruct/tree/binary-search-tree/src/BSTree.hs
+++ algo/datastruct/tree/binary-search-tree/src/BSTree.hs
@@ -21,7 +21,7 @@ module BSTree where
 
 import Test.QuickCheck
 import Data.List (sort)
-import Prelude hiding(lookup)
+import Prelude hiding(lookup, min, max)
 
 data Tree a = Empty 
             | Node (Tree a) a (Tree a) deriving (Show)
@@ -58,54 +58,15 @@ lookup t@(Node l k r) x | k == x = t
                         | otherwise = lookup r x
 
 -- Tree Min
-mint::Tree a -> Tree a
-mint t = if isEmpty (left t) then t else mint $ left t
+min::Tree a -> a
+min (Node Empty x _) = x
+min (Node l _ _) = min l
 
 -- Tree Max
-maxt::Tree a -> Tree a
-maxt t = if isEmpty (right t) then t else maxt $ right t
+max::Tree a -> a
+max (Node _ x Empty) = x
+max (Node _ _ r) = min r
 
--- Below functions are low efficiency compare to their imperactive implementation
-
--- Find parent of a value in tree
--- The performance of parent is low: O(log n)
-parent::(Ord a)=>Tree a -> a -> Tree a
-parent Empty _ = Empty
-parent t x | x < (key t) = if isParent (left t) x then t else parent (left t) x
-           | x > (key t) = if isParent (right t) x then t else parent (right t) x
-           | otherwise = Empty where
-               isParent Empty _ = False
-               isParent t x = (key t)==x
-
--- Find an ancestor of a value wich match a certain rule
--- Performance is very low.
-findAncestor::(Ord a)=>Tree a -> a ->(a->a->Bool)-> Tree a
-findAncestor t x f = checkParent t (parent t x) x f where
-    checkParent _ Empty _ _ = Empty
-    checkParent t p x f = if f (key p) x then p
-                          else checkParent t (parent t (key p)) x f
-
-
--- successor
--- Performance is very low.
-succt::(Ord a)=>Tree a -> a -> Tree a
-succt Empty _ = Empty
-succt t x = if not $ isEmpty rightNode
-            then mint rightNode
-            else findAncestor t x (>) where
-                rightNode = right (lookup t x)
-
--- predecessor
--- Performance is very low
-predt::(Ord a)=>Tree a -> a -> Tree a
-predt Empty _ = Empty
-predt t x = if not $ isEmpty leftNode
-            then maxt leftNode
-            else findAncestor t x (<) where
-                leftNode = left (lookup t x)
-
--- End of low efficiency functions
-              
 -- Insert an element into a tree
 insert::(Ord a) => Tree a -> a -> Tree a
 insert Empty k = Node Empty k Empty
@@ -124,7 +85,7 @@ delete (Node l k r) x | x < k = (Node (delete l x) k r)
                       -- x == k
                       | isEmpty l = r
                       | isEmpty r = l
-                      | otherwise = (Node l k' (delete r k')) where k' = key $ mint r
+                      | otherwise = (Node l k' (delete r k')) where k' = min r
 
 -- Traverse a part of tree inside a range [a, b]
 mapR :: (Ord a)=>(a->b) -> a -> a -> Tree a -> Tree b
@@ -164,12 +125,10 @@ testBuildTree = "\ntest create empty:\t"++ show (Empty::Tree Int)++
 testTreeWalk = "\ntest tree in-order walk by apply (-):\t"++show (mapT negate t2)
 
 -- test min/max for tree
-testMinMax = "\ntest min empty:\t" ++ (show $ mint (Empty::Tree Int)) ++
-             "\ntest min leaf:\t" ++  show (mint t1) ++
-             "\ntest min tree:\t" ++ show (mint t2) ++ 
-             "\ntest max empty:\t" ++ (show $ maxt (Empty::Tree Int)) ++
-             "\ntest max leaf:\t" ++ show (maxt t1) ++
-             "\ntest max tree:\t" ++ show (maxt t2)
+testMinMax = "\ntest min leaf:\t" ++  show (min t1) ++
+             "\ntest min tree:\t" ++ show (min t2) ++ 
+             "\ntest max leaf:\t" ++ show (max t1) ++
+             "\ntest max tree:\t" ++ show (max t2)
 
 -- test lookup in tree
 testLookup = "\ntest lookup empty tree:\t"++ show (lookup (Empty::Tree Int) 3) ++
@@ -177,16 +136,6 @@ testLookup = "\ntest lookup empty tree:\t"++ show (lookup (Empty::Tree Int) 3) +
              "\ntest lookup non exist value in leaf:\t" ++ show (lookup t1 5)++
              "\ntest lookup non exist node in tree:\t"++ show (lookup t2 5)++
              "\ntest lookup a node in tree:\t"++ show (lookup t2 18)
-
--- test succ/pred
-testSuccPred = "\ntest succ of 7:\t"++ show (key (succt t2 7)) ++
-               "\ntest parent of 7:\t"++ show (key (parent t2 7))++
-               "\ntest parent of 13:\t"++ show (key (parent t2 13))++
-               "\ntest bigger ancestor of 7:\t"++ show (key (findAncestor t2 7 (>)))++
-               "\ntest bigger ancestor of 13:\t"++ show (key (findAncestor t2 13 (>)))++
-               "\ntest succ of 13:\t"++ show (key (succt t2 13)) ++
-               "\ntest pred of 6:\t"++ show (key (predt t2 6))++
-               "\ntest pred of 7:\t"++ show (key (predt t2 7))
 
 testDel = "\ntest del 17:\t"++ show (delete t2 17)++
           "\ntest del 7:\t"++ show (delete t2 7)++
@@ -199,7 +148,6 @@ oldTest = do
     putStrLn testMinMax
     putStrLn testLookup
     putStrLn testTreeWalk
-    putStrLn testSuccPred
     putStrLn testDel
 
 --newTest = do
