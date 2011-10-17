991fdb59fa7ad153f5cdd8231762c7ab8e09d7e4
LIU Xinyu <liuxinyu95@gmail.com>
2011-10-17 21:24:50 +0800

    subset sum program, still contains bug.

--- others/problems/subset-sum/subsetsum.py
+++ others/problems/subset-sum/subsetsum.py
@@ -1,25 +1,66 @@
 #!/usr/bin/python
 
+import random
+
 # DP solution based on [1].
 
 # assume xs is monotonically increasing
+class qtab:
+    def __init__(self, n, low, up):
+        self.q = [[False]*(up-low+1)]*n
+        self.l = low
+        self.u = up
+
+    def set(self, i, j, x):
+        self.q[i][j-self.l] = x
+
+    def get(self, i, j):
+        if j<self.l or j>self.u:
+            return False
+        return self.q[i][j-self.l]
+
 def solve(xs, s):
     low = sum([x for x in xs if x < 0])
     up  = sum([x for x in xs if x > 0])
     if s < low or s > up:
         return False
     n = len(xs)
-    q = [[False]*(up-low+1)]*n
+    q = qtab(n, low, up)
     for i in xrange(0, n):
         for j in xrange(low, up+1):
             if i == 0:
-                q[0][j] = xs[0] ==j
+                q.set(i, j, xs[i] == j)
             else:
-                q[i][j] = (q[i-1][j] or (xs[i]==j) or q[i-1][j-xs[i]])
-    return q[n-1][s]
+                q.set(i, j, q.get(i-1, j) or xs[i]==j or q.get(i-1, j-xs[i]))
+    return q.get(n-1, s)
+
+def brute_force(xs, s):
+    if len(xs)==1:
+        return xs[0] == s
+    else:
+        return brute_force(xs[1:], s) or xs[0]==s or brute_force(xs[1:], s-xs[0])
+
+def test():
+    for i in xrange(100):
+        n = random.randint(1, 10)
+        xs = random.sample(xrange(-100, 100), n)
+        l = sum([x for x in xs if x<0])
+        u = sum([x for x in xs if x>0])
+        s = random.randint(l, u)
+        #assert(brute_force(xs, s) == solve(xs, s))
+        __assert(xs, s, brute_force(xs, s), solve(xs, s))
+
+def __assert(xs, s, r1, r2):
+    if r1!=r2:
+        print "xs=", xs, "s=", s
+        print "brute-force=", r1
+        print "dp=", r2
+        assert(r1==r2)
 
 if __name__ == "__main__":
+    #test()
     print solve([-7, -3, -2, 5, 8], 0)
+    print solve([-2, 44, 63], 63)
 
 # Reference
 # [1]. http://en.wikipedia.org/wiki/Subset_sum_problem
