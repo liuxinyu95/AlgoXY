67c2ec6cd94da054c8afddf4f0f137d025e9d452
liuxinyu95 <liuxinyu95@gmail.com>
2012-01-09 13:11:55 +0800

    Fibonacci Heap: Added figures to illustrate Functional Consolidation process.

--- datastruct/heap/other-heaps/create-img.sh
+++ datastruct/heap/other-heaps/create-img.sh
@@ -9,6 +9,14 @@ CONV="src/fr2dot"
 #$CONV img/b4tree.dot "(4, (3, (2, (1, (0)), (0)), (1, (0)), (0)), (2, (1, (0)), (0)), (1, (0)), (0))"
 #$CONV img/link-bitree.dot "(Bk, (Bk, (1), (2), (etc), (k)), (1), (2), (etc), (k))"
 #$CONV img/cut-fib-tree.dot "(new), (r, (p, (x, (x1), (x2), (xe), (xn))), (2), (e), (n))"
+#$CONV img/fib-meld-01.dot "(a, (b)), (c), (d), (e, (f), (g, (h))), (i, (j), (k, (l)), (m, (n), (o, (p)))), (q), (r), (s, (t)), (u, (v), (w, (x)))"
+#$CONV img/fib-meld-02.dot "(c), (a, (b))"
+#$CONV img/fib-meld-03.dot "(a, (b), (c, (d)))"
+#$CONV img/fib-meld-04.dot "(a, (b), (c, (d)), (e, (f), (g, (h))))"
+#$CONV img/fib-meld-05.dot "(a, (b), (c, (d)), (e, (f), (g, (h))), (i, (j), (k, (l)), (m, (n), (o, (p)))))"
+#$CONV img/fib-meld-06.dot "(q), (a, (b), (c, (d)), (e, (f), (g, (h))), (i, (j), (k, (l)), (m, (n), (o, (p)))))"
+#$CONV img/fib-meld-07.dot "(q, (r), (s, (t))), (a, (b), (c, (d)), (e, (f), (g, (h))), (i, (j), (k, (l)), (m, (n), (o, (p)))))"
+
 
 
 #$CONV img/foo.dot "(12), (7, (25)), (15, (28, (41)), (33))"
