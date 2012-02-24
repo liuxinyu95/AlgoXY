01d97d6172d299a6290594d0214a11b2fe28c4c5
liuxinyu95 <liuxinyu95@gmail.com>
2012-02-24 15:40:25 +0800

    Queue: Initialized; Haskell Batched Queue added.

---
+++ datastruct/elementary/queue/create-img.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+CONV="src/fr2dot"
+
+#$CONV img/foo.dot "(12), (7, (25)), (15, (28, (41)), (33))"
+#$CONV img/bheap2.dot "(18), (3, (37)), (6, (8, (30, (45, (55)), (32)), (23, (24)), (22)), (29, (48, (50)), (31)), (10, (17)), (44))"
+
+#$CONV img/b0tree.dot "(0)"
+#$CONV img/b1tree.dot "(1, (0))"
+#$CONV img/b2tree.dot "(2, (1, (0)), (0))"
+#$CONV img/b3tree.dot "(3, (2, (1, (0)), (0)), (1, (0)), (0))"
+#$CONV img/b4tree.dot "(4, (3, (2, (1, (0)), (0)), (1, (0)), (0)), (2, (1, (0)), (0)), (1, (0)), (0))"
+#$CONV img/link-bitree.dot "(Bk, (Bk, (1), (2), (etc), (k)), (1), (2), (etc), (k))"
+#$CONV img/cut-fib-tree.dot "(new), (r, (p, (x, (x1), (x2), (xe), (xn))), (2), (e), (n))"
+#$CONV img/fib-meld-01.dot "(a, (b)), (c), (d), (e, (f), (g, (h))), (i, (j), (k, (l)), (m, (n), (o, (p)))), (q), (r), (s, (t)), (u, (v), (w, (x)))"
+#$CONV img/fib-meld-02.dot "(c), (a, (b))"
+#$CONV img/fib-meld-03.dot "(a, (b), (c, (d)))"
+#$CONV img/fib-meld-04.dot "(a, (b), (c, (d)), (e, (f), (g, (h))))"
+#$CONV img/fib-meld-05.dot "(a, (b), (c, (d)), (e, (f), (g, (h))), (i, (j), (k, (l)), (m, (n), (o, (p)))))"
+#$CONV img/fib-meld-06.dot "(q), (a, (b), (c, (d)), (e, (f), (g, (h))), (i, (j), (k, (l)), (m, (n), (o, (p)))))"
+#$CONV img/fib-meld-07.dot "(q, (r), (s, (t))), (a, (b), (c, (d)), (e, (f), (g, (h))), (i, (j), (k, (l)), (m, (n), (o, (p)))))"
+#$CONV img/pairing-hp.dot "(2, (5, (15)), (4, (13)), (3, (8)), (12), (7), (10), (11), (6), (9, (17), (14, (16))))"
+#$CONV img/pairs.dot "(5, (15)), (4, (13)), (3, (8)), (12), (7), (10), (11), (6), (9, (17), (14, (16)))"
+#$CONV img/pairs-merge.dot "(4, (5, (15)), (13)), (3, (12), (8)), (7, (10)), (6, (11)), (9, (7), (14, (16)))"
+#$CONV img/right-merge-1.dot "(6, (9, (7), (14, (16))), (11))"
+#$CONV img/right-merge-2.dot "(6, (7, (10)), (9, (14, (16))), (11))"
+#$CONV img/right-merge-3.dot "(3, (6, (7, (10)), (9, (14, (16))), (11)), (12), (8))"
+#$CONV img/right-merge-4.dot "(3, (4, (5, (15)), (13)), (6, (7, (10)), (9, (14, (16))), (11)), (12), (8))"
