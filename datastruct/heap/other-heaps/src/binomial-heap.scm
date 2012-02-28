caecf78e04992c05ded9e3fc958853709defe747
liuxinyu95 <liuxinyu95@gmail.com>
2012-02-28 14:12:50 +0800

    binomial-heap.scm: updated year information.

--- datastruct/heap/other-heaps/src/binomial-heap.scm
+++ datastruct/heap/other-heaps/src/binomial-heap.scm
@@ -1,6 +1,6 @@
 #|
  binomial-heap.scm Binomial Heap in Scheme/Lisp
- Copyright (C) 2010 Liu Xinyu (liuxinyu95@gmail.com)
+ Copyright (C) 2012 Liu Xinyu (liuxinyu95@gmail.com)
  
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
