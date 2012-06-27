c8c6421e6db93b382e9136b83a44bff9f1f279a6
liuxinyu95 <liuxinyu95@gmail.com>
2012-06-27 13:18:18 +0800

    Added BRAList definition and cons parts

--- datastruct/elementary/sequence/create-img.sh
+++ datastruct/elementary/sequence/create-img.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/sh
 
-CONV="../../heap/other-heap/src/Fr2Dot"
+CONV="../../heap/other-heaps/src/Fr2Dot"
 
-#$CONV img/foo.dot "(12), (7, (25)), (15, (28, (41)), (33))"
-#$CONV img/bheap2.dot "(18), (3, (37)), (6, (8, (30, (45, (55)), (32)), (23, (24)), (22)), (29, (48, (50)), (31)), (10, (17)), (44))"
-
-#$CONV img/b0tree.dot "(0)"
-#$CONV img/b1tree.dot "(1, (0))"
-#$CONV img/b2tree.dot "(2, (1, (0)), (0))"
-#$CONV img/b3tree.dot "(3, (2, (1, (0)), (0)), (1, (0)), (0))"
-#$CONV img/b4tree.dot "(4, (3, (2, (1, (0)), (0)), (1, (0)), (0)), (2, (1, (0)), (0)), (1, (0)), (0))"
-#$CONV img/link-bitree.dot "(Bk, (Bk, (1), (2), (etc), (k)), (1), (2), (etc), (k))"
-#$CONV img/cut-fib-tree.dot "(new), (r, (p, (x, (x1), (x2), (xe), (xn))), (2), (e), (n))"
+$CONV img/bi-tree-sequence.dot "(t1, (x1), (x2)), (t2, (dot1, (x3), (x4)), (dot2, (x5), (x6)))"
