38b2017b4c09914959ecad37018ad8a2ab93c715
liuxinyu95 <liuxinyu95@gmail.com>
2012-08-29 13:44:25 +0800

    Finished paired-array list in C++

--- datastruct/elementary/sequence/src/palist.cpp
+++ datastruct/elementary/sequence/src/palist.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-#define BIG_RAND() (rand() % 20)
+#define BIG_RAND() (rand() % 1000)
 
 template<typename Key>
 struct List {
@@ -21,6 +21,15 @@ struct List {
 
   List() : n(0), m(0) {}
   int size() { return n + m; }
+
+  void print() {
+    printf("n=%d, m=%d\n", n, m);
+    cout<<"front:";
+    copy(front.begin(), front.end(), ostream_iterator<Key>(cout, ", "));
+    cout<<"\nrear: ";
+    copy(rear.begin(), rear.end(), ostream_iterator<Key>(cout, ", "));
+    cout<<"\n";
+  }
 };
 
 /* Insertion and appending */
@@ -44,23 +53,41 @@ void append(List<Key>& xs, Key x) {
 template<typename Key>
 void balance(List<Key>& xs) {
   if(xs.n == 0) {
-    //front = reverse_copy(rear[0 ~ m/2]);
-    //rear = copy(rear[m/2+1 ~ m-2])
+    back_insert_iterator<vector<Key> > i(xs.front);
+    reverse_copy(xs.rear.begin(), xs.rear.begin() + (xs.m >> 1), i);
+    xs.rear.erase(xs.rear.begin(), xs.rear.begin() +(xs.m >> 1));
+    xs.n = (xs.m >> 1);
+    xs.m -= xs.n;
+  }
+  else if(xs.m == 0) {
+    swap(xs.front, xs.rear);
+    swap(xs.n, xs.m);
+    balance(xs);
+    swap(xs.front, xs.rear);
+    swap(xs.n, xs.m);
   }
 }
 
 template<typename Key>
 void remove_head(List<Key>& xs) {
-  if(xs.n == 0)
-    balance(xs);
-  xs.front.pop_back();
+  balance(xs);
+  if(xs.front.empty())
+    remove_tail(xs); //remove the singleton elem in rear
+  else {
+    xs.front.pop_back();
+    --xs.n;
+  }
 }
 
 template<typename Key>
 void remove_tail(List<Key>& xs) {
-  if(xs.m == 0)
-    balance(ys);
-  xs.rear.pop_back();
+  balance(xs);
+  if(xs.rear.empty())
+    remove_head(xs); //remove the singletone elem in front
+  else {
+    xs.rear.pop_back();
+    --xs.m;
+  }
 }
 
 /* Random accessing, skip the out-of-bound error handling*/
@@ -95,33 +122,60 @@ void print_list(List<Key> xs) {
     printf( i==xs.size() -1 ? "%d\n" : "%d, ", get(xs, i));
 }
 
+void print_sample(deque<int>& xs, List<int>& ys) {
+  cout<<"\ndeque:\n";
+  copy(xs.begin(), xs.end(), ostream_iterator<int>(cout, ", "));
+  cout<<"\npalist:\n";
+  print_list(ys);
+}
+
+int sample(deque<int>& xs, List<int>& ys) {
+  int n = BIG_RAND() + 1;
+  for(int i=0; i<n; ++i) {
+    int x = BIG_RAND();
+    if(BIG_RAND() % 2) {
+      xs.push_back(x);
+      append(ys, x);
+    }
+    else {
+      xs.push_front(x);
+      insert(ys, x);
+    }
+  }
+  return n;
+}
+
 void test_insert() {
   for(int m=0; m<100; ++m) {
-    int n = BIG_RAND();
     deque<int> xs;
     List<int> ys;
-    for(int i=0; i<n; ++i) {
-      int x = BIG_RAND();
-      if(BIG_RAND()%2) {
-        xs.push_back(x);
-        append(ys, x);
+    sample(xs, ys);
+    assert_eq(xs, ys);
+  }
+}
+
+void test_remove() {
+  for(int m=0; m<100; ++m) {
+    deque<int> xs;
+    List<int> ys;
+    int n = sample(xs, ys);
+    int c = rand() % n + 1;
+    for(int i=0; i<c; ++i) {
+      if(BIG_RAND() % 2) {
+        xs.pop_back();
+        remove_tail(ys);
       }
-      else {
-        xs.push_front(x);
-        insert(ys, x);
+      else{
+        xs.pop_front();
+        remove_head(ys);
       }
     }
-    /*
-    cout<<"\ndeque:\n";
-    copy(xs.begin(), xs.end(), ostream_iterator<int>(cout, ", "));
-    cout<<"\npalist:\n";
-    print_list(ys);
-    */
     assert_eq(xs, ys);
   }
 }
 
 int main(int, char**) {
   test_insert();
+  test_remove();
   return 0;
 }
