72512d1469cfdded0ddc9cc13db2cf934a4d9605
liuxinyu95 <liuxinyu95@gmail.com>
2012-10-22 19:04:11 +0800

    Added length calculating for list

--- others/appendix/list/src/list.cpp
+++ others/appendix/list/src/list.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cstdlib>
 
 template<typename T>
 struct List {
   T key;
   List* next;
+
+  List() : next(0) {}
+  ~List() { delete next; }
 };
 
 template<typename T>
@@ -18,4 +22,18 @@ List<T>* cons(T x, List<T>* xs) {
   lst->key = x;
   lst->next = xs;
   return lst;
+}
+
+
+template<typename T>
+int length(List<T>* xs) {
+  int n = 0;
+  for (; xs; ++n, xs = xs->next);
+  return n;
+}
+
+int main(int, char**) {
+  List<int>* lst = cons(1, cons(2, cons<int>(3, NULL)));
+  printf("length of empty = %d, len([1, 2, 3]) = %d\n", length<int>(0), length(lst));
+  delete lst;
 }
