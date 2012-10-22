aee6c9375b67986c6326a28405057536a71bc45c
liuxinyu95 <liuxinyu95@gmail.com>
2012-10-22 19:04:11 +0800

    Added length calculating for list

--- others/appendix/list/src/list-tmp.cpp
+++ others/appendix/list/src/list-tmp.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
 
 struct Empty;
 
-template<n, T> struct List {
+template<int n, typename T> struct List {
   static const int first = n;
   typedef T rest;
 };
+
+template<typename L> struct Length{
+  static const int value = 1 + Length<typename L::rest>::value;
+};
+
+template<> struct Length<Empty> {
+  static const int value = 0;
+};
+
+
+int main(int, char**) {
+  typedef List<1, List<2, List<3, Empty> > > Lst;
+  std::cout<<Length<Empty>::value<<"\n"<<Length<Lst>::value<<"\n";
+}
