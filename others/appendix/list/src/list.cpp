d69c5f2e67292e47ab3eed8b3b263a25671b8d7b
liuxinyu95 <liuxinyu95@gmail.com>
2012-10-24 13:42:38 +0800

    Added appending part

--- others/appendix/list/src/list.cpp
+++ others/appendix/list/src/list.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 
+using namespace std;
+
 template<typename T>
 struct List {
   T key;
@@ -8,6 +10,15 @@ struct List {
 
   List() : next(0) {}
   ~List() { delete next; }
+};
+
+template<typename T>
+struct List1 {
+  List<T>* head;
+  List<T>* tail;
+
+  List1() : head(0), tail(0) {}
+  ~List1() { delete head; }
 };
 
 template<typename T>
@@ -24,7 +35,6 @@ List<T>* cons(T x, List<T>* xs) {
   return lst;
 }
 
-
 template<typename T>
 int length(List<T>* xs) {
   int n = 0;
@@ -32,8 +42,30 @@ int length(List<T>* xs) {
   return n;
 }
 
+template<typename T>
+List<T>* append(List<T>* xs, T x) {
+  List<T> *tail, *head;
+  for (head = tail = xs; xs; xs = xs->next)
+    tail = xs;
+  if (!head)
+    head = cons<T>(x, NULL);
+  else
+    tail->next = cons<T>(x, NULL);
+  return head;
+}
+
+template<typename T>
+void print_lst(List<T>* xs) {
+  for(; xs; xs = xs->next)
+    cout<<xs->key<<", ";
+  cout<<"\n";
+}
+
 int main(int, char**) {
   List<int>* lst = cons(1, cons(2, cons<int>(3, NULL)));
   printf("length of empty = %d, len([1, 2, 3]) = %d\n", length<int>(0), length(lst));
+  lst = append(lst, 4);
+  printf("append 4 ==>");
+  print_lst(lst);
   delete lst;
 }
