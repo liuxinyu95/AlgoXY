d7b19c13eb5c15110b7bad659b7559d5140b48d7
liuxinyu95 <liuxinyu95@gmail.com>
2012-11-06 13:41:29 +0800

    Added insertion

--- others/appendix/list/src/ListEx.hs
+++ others/appendix/list/src/ListEx.hs
@@ -8,5 +8,13 @@ atR xs i = get xs (drop i xs) where
   get (x:_) [_] = x
   get (_:xs) (_:ys) = get xs ys
   
+insertAt :: [a] -> Int -> a -> [a]
+insertAt xs 0 y = y:xs
+insertAt [] i y = [y]
+insertAt (x:xs) i y = x : insertAt xs (i-1) y
+
 prop_rindex :: [Int] -> Bool
 prop_rindex xs = xs == (map (atR xs) $ reverse [0..length xs -1])
+
+prop_insertAt :: [Int] -> Int -> Int -> Property
+prop_insertAt xs i x = (0 <= i) ==> (insertAt xs i x) == (let (as, bs) = splitAt i xs in as ++ x:bs)
