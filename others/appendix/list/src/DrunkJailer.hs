a7cba154ecd1be9f2d991c7d0cb58be1b9ccc8eb
liuxinyu95 <liuxinyu95@gmail.com>
2012-11-23 14:12:34 +0800

    Added a raw native solution without using folding, zipping etc

--- others/appendix/list/src/DrunkJailer.hs
+++ others/appendix/list/src/DrunkJailer.hs
@@ -5,10 +5,15 @@ module DrunkJailer where
 -- Naive brute force ver.
 solve n = sum $ map snd $ foldl proc (zip [1..n] (repeat 0)) [1..n] where
     proc xs i = map (switch i) xs
-    switch i (j, x) = if j `mod` i == 0 then (j, 1 - x) else (j, x)
+                
+switch i (j, x) = if j `mod` i == 0 then (j, 1 - x) else (j, x)
 
 -- Naive ver, without explicitly using folding, and zip
---solve' n = sum (map snd (proc [1..n]) where
+solve' = sum . (map snd) . proc  where
+    proc n = operate [1..n] $ map (\i -> (i, 0)) [1..n]
+    operate [] xs = xs
+    operate (i:is) xs = operate is (map (switch i) xs)
 
 -- induction
--- map solve [1..100]
+test = map solve [1..100]
+test' = map solve' [1..100]
