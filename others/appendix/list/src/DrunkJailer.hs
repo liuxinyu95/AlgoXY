ea6fbec6c5f08378fada8beba88bfcd96484d6ab
liuxinyu95 <liuxinyu95@gmail.com>
2012-11-26 10:01:10 +0800

    Added proof for the drunk jailer puzzle

--- others/appendix/list/src/DrunkJailer.hs
+++ others/appendix/list/src/DrunkJailer.hs
@@ -23,6 +23,20 @@ solve'' = floor . sqrt . fromIntegral
 test = map solve [1..100]
 test' = map solve' [1..100]
 
+-- induction for which cells(lights) are on
+--   results 100 ==> [1,4,9,16,25,36,49,64,81,100]
+results n = map fst $ filter (\x -> 1 == snd x) $ foldl proc (zip [1..n] (repeat 0)) [1..n] where
+    proc xs i = map (switch i) xs
+                
+-- Key fact:
+--   The numbers which have odd number of factors is left as 1.
+--   Only perfect square numbers have odd number of factors.
+--      Proof: for any positive number N, if p is factor of N, there
+--      exists integer q that, N = p * q, so we add 2 factors to the set of factors of N
+--      if and only if p != q. otherwise, p = q leads to perfect squre number, and  
+--      we add 1 to the set of factors, so that
+--      the size of factor set is odd.
+                
 -- test
 prop_solve :: Int -> Bool
 prop_solve n = let m = if n `elem` [1..1000] then abs(n) else 1 in solve m == solve'' m
