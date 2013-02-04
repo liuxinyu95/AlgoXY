7dc6e0973f094605b890dcdb958eaac5ecebea11
liuxinyu95 <liuxinyu95@gmail.com>
2013-02-04 13:50:07 +0800

    Started adding tournament tree Haskell program

--- sorting/select-sort/src/ttree.c
+++ sorting/select-sort/src/ttree.c
@@ -1,4 +1,21 @@
 /*
+ * ttree.c
+ * Copyright (C) 2013 Liu Xinyu (liuxinyu95@gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
  * Tournament tree based selection sort
  * [1] Donald E. Knuth. ``The Art of Computer Programming, Volume 3: Sorting and Searching (2nd Edition)''. 
  * Addison-Wesley Professional; 2 edition (May 4, 1998) ISBN-10: 0201896850 ISBN-13: 978-0201896855
