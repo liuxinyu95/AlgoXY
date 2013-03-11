7747752663a9b1a5b33d4fe5075097c518ac413a
liuxinyu95 <liuxinyu95@gmail.com>
2013-03-11 19:08:10 +0800

    Added quick sort program based on R. Sedgewick method

--- sorting/quick-sort/src/qsort.c
+++ sorting/quick-sort/src/qsort.c
@@ -5,8 +5,8 @@
 
 typedef int Key;
 
-#define swap(x, y) { Key tmp = (x); (x) = (y); (y) = tmp; }
 #define N 100000
+#define swap(x, y) { Key tmp = (x); (x) = (y); (y) = tmp; }
 
 void printrange(Key* xs, int l, int u) {
     printf("xs[%d, %d) = ", l, u);
@@ -42,24 +42,70 @@ void quicksort(Key* xs, int l, int u) {
     }
 }
 
+/*
+ * Robert Sedgewick partition algorithm
+ * Based on: 
+ * Robert Sedgewick. ``Implementing quick sort programs''. Communication of ACM. pp.847 - 857. Volumn 21. Number 10. 1978.
+ */
+
+void qsort1(Key* xs, int l, int u) {
+    int pivot, i, j;
+    if (l < u) {
+        pivot = xs[l]; i = l; j = u;
+        while (1) {
+            do { ++i; } while (xs[i] < pivot);
+            do { --j; } while (pivot < xs[j]);
+            if (j < i) break;
+            swap(xs[i], xs[j]);
+        }
+        swap(xs[l], xs[j]);
+        qsort1(xs, l, j);
+        qsort1(xs, i, u);
+    }
+}
+
+/*
+ * An variant of Sedgewick sort algorithm.
+ */
+void exchange(Key* xs, int i, int j) {
+    Key tmp = xs[i]; xs[i] = xs[j]; xs[j] = tmp;
+}
+
+void qsort2(Key* xs, int l, int u) {
+    int p, i, j;
+    if (l < u) {
+        for (p = xs[l], i = l + 1, j = u - 1; ;exchange(xs, i++, j--)) {
+            while (xs[i] < p) ++i;
+            while (p < xs[j]) --j;
+            if (j < i) 
+                break;
+        }
+        swap(xs[l], xs[j]);
+        qsort2(xs, l, j);
+        qsort2(xs, i, u);
+    }
+}
+
 /* test */
 Key cmp(const void* x, const void* y) {
     return *(Key*)x - *(Key*)y;
 }
 
-void testqsort() {
+void testqsort(void (*fsort)(Key*, int, int)) {
     int i, j, n, xs[N], ys[N];
     for (j = 0; j < 100; ++j) {
         for (i = 0, n = rand() % N; i < n; ++i)
             xs[i] = rand() % N;
         memcpy((void*)ys, (const void*)xs, n * sizeof(int));
         qsort(xs, n, sizeof(int), cmp);
-        quicksort(ys, 0, n);
+        fsort(ys, 0, n);
         assert(!memcmp(xs, ys, n * sizeof(int)));
     }
 }
 
 int main() {
-    testqsort();
+    testqsort(quicksort);
+    testqsort(qsort1);
+    testqsort(qsort2);
     return 0;
 }
