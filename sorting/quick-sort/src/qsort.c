6bda87c850a68bbb3ec82db5017143fd09b7f925
liuxinyu95 <liuxinyu95@gmail.com>
2013-03-12 13:49:06 +0800

    A working tenery quick sort C program

--- sorting/quick-sort/src/qsort.c
+++ sorting/quick-sort/src/qsort.c
@@ -5,7 +5,8 @@
 
 typedef int Key;
 
-#define N 100000
+//#define N 100000
+#define N 20
 #define swap(x, y) { Key tmp = (x); (x) = (y); (y) = tmp; }
 
 void printrange(Key* xs, int l, int u) {
@@ -20,13 +21,13 @@ void printrange(Key* xs, int l, int u) {
  * negate of less than is enough for strict weak order
  */
 int partition(Key* xs, int l, int u) {
-    int pivot, r;
-    for (pivot = l, r = l + 1; r < u; ++r)
-        if (!(xs[pivot] < xs[r])) {
+    int r; Key pivot = xs[l];
+    for (r = l + 1; r < u; ++r)
+        if (!(pivot < xs[r])) {
             ++l;
             swap(xs[l], xs[r]);
         }
-    swap(xs[pivot], xs[l]);
+    swap(pivot, xs[l]);
     return l + 1;
 }
 
@@ -49,39 +50,49 @@ void quicksort(Key* xs, int l, int u) {
  */
 
 void qsort1(Key* xs, int l, int u) {
-    int pivot, i, j;
+    int i, j; Key pivot;
     if (l < u) {
-        pivot = xs[l]; i = l; j = u;
+        i = l; j = u; pivot = xs[l];
         while (1) {
-            do { ++i; } while (xs[i] < pivot);
-            do { --j; } while (pivot < xs[j]);
-            if (j < i) break;
+            while (i < u && xs[++i] < pivot);
+            while (j >=l && pivot < xs[--j]);
+            if (j <= i) break;
             swap(xs[i], xs[j]);
         }
-        swap(xs[l], xs[j]);
+        swap(pivot, xs[j]);
         qsort1(xs, l, j);
         qsort1(xs, i, u);
     }
 }
 
 /*
- * An variant of Sedgewick sort algorithm.
+ * Ternery quick sort (3-way partition)
+ * Based on:
+ * Jon Bentley, M. Douglas McILROY. ``Engineering a sort function''. Software Practice and experience VOL. 23(11), 1249-1265 1993.
  */
 void exchange(Key* xs, int i, int j) {
     Key tmp = xs[i]; xs[i] = xs[j]; xs[j] = tmp;
 }
 
 void qsort2(Key* xs, int l, int u) {
-    int p, i, j;
+    int i, j, k, p, q; Key pivot;
     if (l < u) {
-        for (p = xs[l], i = l + 1, j = u - 1; ;exchange(xs, i++, j--)) {
-            while (xs[i] < p) ++i;
-            while (p < xs[j]) --j;
-            if (j < i) 
-                break;
+        i = p = l; j = q = u; pivot = xs[l];
+        while (i < j) {
+            while (i < u && xs[++i] < pivot);
+            while (j >= 0 && pivot < xs[--j]);
+            if (j < i) break;
+            exchange(xs, i, j);
+            if (xs[i] == xs[pivot])
+                exchange(xs, ++p, i);
+            if (xs[j] == xs[pivot])
+                exchange(xs, --q, j);
         }
-        swap(xs[l], xs[j]);
-        qsort2(xs, l, j);
+        for (k = l; k <= p; ++k, --j)
+            exchange(xs, k, j);
+        for (k = u-1; k >= q; --k, ++i)
+            exchange(xs, k, i);
+        qsort2(xs, l, j+1);
         qsort2(xs, i, u);
     }
 }
@@ -97,15 +108,27 @@ void testqsort(void (*fsort)(Key*, int, int)) {
         for (i = 0, n = rand() % N; i < n; ++i)
             xs[i] = rand() % N;
         memcpy((void*)ys, (const void*)xs, n * sizeof(int));
+        printrange(xs, 0, n);
         qsort(xs, n, sizeof(int), cmp);
         fsort(ys, 0, n);
-        assert(!memcmp(xs, ys, n * sizeof(int)));
+        if (memcmp(xs, ys, n * sizeof(int))) {
+            printf("error\n");
+            printrange(xs, 0, n);
+            printrange(ys, 0, n);
+            exit(1);
+        }
+        //assert(!memcmp(xs, ys, n * sizeof(int)));
     }
 }
 
 int main() {
-    testqsort(quicksort);
-    testqsort(qsort1);
+    //testqsort(quicksort);
+    //testqsort(qsort1);
     testqsort(qsort2);
+    const int xs[] = {9, 0, 5}; //{9, 13, 18, 10, 12, 4, 18};
+    int ys[3];
+    memcpy((void*)ys, (const void*)xs, 3*sizeof(int));
+    qsort2(ys, 0, 3);
+    printrange(ys, 0, 3);
     return 0;
 }
