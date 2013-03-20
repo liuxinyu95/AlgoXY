e61b71ef7ab0a468c44343607e4551087044e908
liuxinyu95 <liuxinyu95@gmail.com>
2013-03-20 12:40:00 +0800

    Provide an unique self-contained merge sort Haskell program

--- sorting/merge-sort/src/MergeSort.hs
+++ sorting/merge-sort/src/MergeSort.hs
@@ -1,4 +1,4 @@
--- MergeSort.hs
+-- MergeSort1.hs
 -- Copyright (C) 2012 Liu Xinyu (liuxinyu95@gmail.com)
 -- 
 -- This program is free software: you can redistribute it and/or modify
@@ -16,14 +16,41 @@
 
 module MergeSort where
 
+import Test.QuickCheck
+import Data.List
+
+-- Basic version, split at the middle point
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort as) (msort bs) where
+  (as, bs) = splitAt (length xs `div` 2) xs
+
+
+-- Basic version, split odd/even
+
+mergesort [] = []
+mergesort [x] = [x]
+mergesort xs = merge (mergesort as) (mergesort bs) where
+    (as, bs) = split xs
+
+split [] = ([], [])
+split [x] = ([x], [])
+split (x:y:xs) = (x:xs', y:ys') where (xs', ys') = split xs
+
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) | x <= y = x : merge xs (y:ys)
+                    | x >  y = y : merge (x:xs) ys
+
+-- A version for performance visualization
 -- This version record the number of swaps which happens during sorting.
 
 --merge :: (Ord a)=>([a], Int)->([a], Int) -> ([a], Int)
-merge ([], n) (ys, m) = (ys, n+m)
-merge (xs, n) ([], m) = (xs, n+m)
-merge ((x:xs), n) ((y:ys), m) = 
-    if x< y then let (xs', n') = merge (xs, n) ((y:ys), m) in (x:xs', n')
-    else let (xs', n') = merge ((x:xs), n) (ys, m) in (y:xs', n'+1)
+merge' ([], n) (ys, m) = (ys, n+m)
+merge' (xs, n) ([], m) = (xs, n+m)
+merge' ((x:xs), n) ((y:ys), m) = 
+    if x< y then let (xs', n') = merge' (xs, n) ((y:ys), m) in (x:xs', n')
+    else let (xs', n') = merge' ((x:xs), n) (ys, m) in (y:xs', n'+1)
 
 --sort' :: (Ord a)=>[([a], Int)] -> ([a], Int)
 sort' [] = ([], 0)
@@ -33,9 +60,11 @@ sort' xss = sort' $ mergePairs xss
 --mergePairs :: (Ord a)=>[([a], Int)]->[([a], Int)]
 mergePairs [] = []
 mergePairs [xs] = [xs]
-mergePairs (xs:ys:xss) = merge xs ys : mergePairs xss
+mergePairs (xs:ys:xss) = merge' xs ys : mergePairs xss
 
---sort :: (Ord a)=>[a]->([a], Int)
-sort = sort' . map (\x->([x], 0))
+msort' :: (Ord a)=>[a]->([a], Int)
+msort' = sort' . map (\x->([x], 0))
 
-test = sort [5, 9, 1, 0, 5, 4, 3, 1, 2, 3]
+--test
+prop_msort :: [Int] -> Bool
+prop_msort xs = sort xs == (msort xs)
