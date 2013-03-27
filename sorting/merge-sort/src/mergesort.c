04ef1e2285b89a7d1f24261ba02e63532c7ff45d
liuxinyu95 <liuxinyu95@gmail.com>
2013-03-27 13:52:29 +0800

    Implemented in-place merge sort

--- sorting/merge-sort/src/mergesort.c
+++ sorting/merge-sort/src/mergesort.c
@@ -92,6 +92,8 @@ void msort2(Key* xs, int l, int u) {
 /*
  * Naive in-place merge
  * Refer to http://penguin.ewu.edu/cscd300/Topic/AdvSorting/MergeSorts/InPlace.html
+ *
+ * invariant:  ...merged...| xs[i] ... sorted sub A ... | xs[j] ... sorted sub B ...
  */
 void naive_merge(Key* xs, int l, int m, int u) {
     int i; Key y;
@@ -114,6 +116,64 @@ void msort3(Key* xs, int l, int u) {
     }
 }
 
+/*
+ * A in-placed version based on:
+ * Jyrki Katajainen, Tomi Pasanen, Jukka Teuhola. ``Practical in-place mergesort''. Nordic Journal of Computing, 1996.
+ */
+void swap(Key* xs, int i, int j) {
+    Key tmp = xs[i]; xs[i] = xs[j]; xs[j] = tmp;
+}
+
+/*
+ * merge two sorted subs xs[i, m) and xs[j...n) to working area xs[w...] 
+ */
+void wmerge(Key* xs, int i, int m, int j, int n, int w) {
+    while (i < m && j < n)
+        swap(xs, w++, xs[i] < xs[j] ? i++ : j++);
+    while (i < m)
+        swap(xs, w++, i++);
+    while (j < n)
+        swap(xs, w++, j++);
+}
+
+void imsort(Key* xs, int l, int u);
+
+/* 
+ * sort xs[l, u), and put result to working area w. 
+ * constraint, len(w) == u - l
+ */
+void wsort(Key* xs, int l, int u, int w) {
+    int m;
+    if (u - l > 1) {
+        m = l + (u - l) / 2;
+        imsort(xs, l, m);
+        imsort(xs, m, u);
+        wmerge(xs, l, m, m, u, w);
+    }
+    else
+        while (l < u)
+            swap(xs, l++, w++);
+}
+
+void imsort(Key* xs, int l, int u) {
+    int m, n, w;
+    if (u - l > 1) {
+        m = l + (u - l) / 2;
+        w = l + u - m;
+        wsort(xs, l, m, w); /* the last half contains sorted elements */
+        while (w - l > 2) {
+            n = w;
+            m = l + (n - l + 1) / 2;
+            wsort(xs, m, n, l);  /* the first half of the previous working area contains sorted elements */
+            w = m;
+            wmerge(xs, l, l + n - m, n, u, m);
+        }
+        for (n = w; n > l; --n) /*switch to insertion sort*/
+            for (m = n; m < u && xs[m] < xs[m-1]; ++m)
+                swap(xs, m, m - 1);
+    }
+}
+
 /* test */
 int cmp(const void* x, const void* y) {
     return *(Key*)x - *(Key*)y;
@@ -127,14 +187,12 @@ void testmsort(void (*fsort)(Key*, int, int)) {
         memcpy((void*)ys, (const void*)xs, n * sizeof(int));
         qsort(xs, n, sizeof(int), cmp);
         fsort(ys, 0, n);
-        assert(!memcmp(xs, ys, n * sizeof(int)));
-        /*
+        //assert(!memcmp(xs, ys, n * sizeof(int)));
         if(memcmp(xs, ys, n * sizeof(int))) {
             printrange(xs, 0, n);
             printrange(ys, 0, n);
             exit(-1);
         }
-        */
     }
 }
 
@@ -144,13 +202,21 @@ int main() {
     testmsort(msort);
     printf("basic version tested, average time: %f\n", (clock() - t) / CLOCKS_PER_SEC / 100.0);
     */
+
+    /*
     t = clock();
     testmsort(msort2);
     printf("working area version tested, average time: %f\n", (clock() - t) / CLOCKS_PER_SEC / 100.0);
+    */
 
     /* Don't use big N such as 100,000, it's very slow, for 10,000, average tm = 0.022948
     testmsort(msort3);
     printf("naive in-place version tested, average time: %f.\n", (clock() - t) / CLOCKS_PER_SEC / 100.0);
     */
+
+    t = clock();
+    testmsort(imsort);
+    printf("in-place workarea version tested, average time: %f\n", (clock() - t) / CLOCKS_PER_SEC / 100.0);
+
     return 0;
 }
