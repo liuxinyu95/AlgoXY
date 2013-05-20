b4ebd54e3ee384de9f3e122fae5aef2c52bc925b
liuxinyu95 <liuxinyu95@gmail.com>
2013-05-20 13:11:07 +0800

    Added boyer-moore majority algorithm in C

--- search/scan/majority-vote/mvote.cpp
+++ search/scan/majority-vote/mvote.cpp
@@ -11,7 +11,7 @@ using namespace std;
 /* A brute force solution. */
 
 template<typename T>
-T majority(const T* xs, int n, T fail) {
+T majority_brute(const T* xs, int n, T fail) {
     map<T, int> m;
     int i, max = 0;
     T r;
@@ -25,11 +25,30 @@ T majority(const T* xs, int n, T fail) {
     return max * 2 >= n ? r : fail;
 }
 
+/* Boyer-Moore algorithm. */
+template<typename T>
+T majority(const T* xs, int n, T fail) {
+    T m;
+    int i, c;
+    for (i = 1, m = xs[0], c = 1; i < n; ++i) {
+        c += xs[i] == m ? 1 : -1;
+        if (c < 0) {
+            m = xs[i];
+            c = 0;
+        }
+    }
+    for (i = 0, c = 0; i < n; ++i, c += xs[i] == m);
+    return c * 2 >= n ? m : fail;
+}
+
 void test() {
     const char xs[] = {'A', 'A', 'A', 'C', 'C', 'B', 'B', 'C', 'C', 'C', 'B', 'C', 'C'};
     const int ys[] = {1, 2, 3, 1, 2, 3};
+    printf("majority_brute = %c\n", majority_brute(xs, sizeof(xs)/sizeof(xs[0]), 'X'));
     printf("majority = %c\n", majority(xs, sizeof(xs)/sizeof(xs[0]), 'X'));
+    printf("majority_brute = %d\n", majority_brute(ys, sizeof(ys)/sizeof(ys[0]), -1));
     printf("majority = %d\n", majority(ys, sizeof(ys)/sizeof(ys[0]), -1));
+
 }
 
 int main() {
