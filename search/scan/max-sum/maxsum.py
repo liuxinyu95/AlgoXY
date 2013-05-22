42c9272c3dd55f5e788b27ac24cbb2a424db0d58
liuxinyu95 <liuxinyu95@gmail.com>
2013-05-22 13:02:21 +0800

    Added max sum python program

--- search/scan/max-sum/maxsum.py
+++ search/scan/max-sum/maxsum.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+# refer to column 7 in [1]
+# [1]. Jon Bentley. ``Programming pearls, Second Edition''. Addison-Wesley Professional; 1999. ISBN-13: 978-0201657883
+
+import random
+
 # invariant
 # .... max so far .... max end here
 def maxsum(xs):
@@ -8,6 +13,31 @@ def maxsum(xs):
     for x in xs:
         m = max(m + x, 0)
         s = max(m, s)
+    return s
 
 def naive_maxsum(xs):
-    pass
+    n = len(xs)
+    m = 0
+    r = (0, 0)
+    for i in range(n):
+        s = 0
+        for j in range(i, n):
+            s = s + xs[j]
+            if m < s:
+                m = s
+                r = (i, j)
+    return (m, r)
+
+def test():
+    for _ in range(100):
+        xs = random.sample(range(-100, 100), 100)
+        (m, _) = naive_maxsum(xs)
+        assert m == maxsum(xs)
+
+def example():
+    xs = [3, -13, 19, -12, 1, 9, 18, -16, 15, -15]
+    print naive_maxsum(xs)
+
+if __name__ == "__main__":
+    example()
+    test()
