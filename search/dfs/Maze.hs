096e078f378e34fa0abddcc8737c6af33ca9fa9a
Larry Liu <larry.1.liu@nokia.com>
2013-09-22 17:27:14 +0800

    Added the purely functional DFS Haskell program, it only returns the first solution. Next is to develop a program returns all solution.

--- search/dfs/Maze.hs
+++ search/dfs/Maze.hs
@@ -1,6 +1,7 @@
 module Maze where
 
 import Data.Array
+import Debug.Trace (trace)
 
 -- This is rather like exhaustive search than DFS
 solveMaze m from to = map reverse $ solve from [[]] where
@@ -12,7 +13,7 @@ solveMaze m from to = map reverse $ solve from [[]] where
     visited p paths = any (p `elem`) paths
 
 -- This just DFS VISIT the maze, but not work for a solution.
-dfsSolve m from to = reverse $ solve [from] [] where
+dfsVisit m from to = reverse $ solve [from] [] where
     solve [] path = path
     solve (c:cs) path 
         | c == to = c:path
@@ -21,16 +22,29 @@ dfsSolve m from to = reverse $ solve [from] [] where
     adjacent (x, y) = [(x', y') | (x', y') <- [(x-1, y), (x+1, y), (x, y-1), (x, y+1)], 
                                   inRange (bounds m) (x', y'), m ! (x', y') == 0]
 
+-- DFS solve, return ONLY the first solution
+dfsSolve m from to = reverse $ solve [[from]] [] where
+    solve [] visit = []
+    solve (c@(p:path):cs) visit
+        | p == to = c -- stop at the first solution
+        | otherwise = let os = filter (`notElem` visit) (adjacent p) in
+                      if os == [] 
+                      then solve cs visit
+                      else solve ((map (:c) os) ++ cs) (p:visit) -- try new candidates
+    adjacent (x, y) = [(x', y') | (x', y') <- [(x-1, y), (x+1, y), (x, y-1), (x, y+1)], 
+                                  inRange (bounds m) (x', y'), m ! (x', y') == 0]
 
 mz = [[0, 0, 1, 0, 1, 1],
       [1, 0, 1, 0, 1, 1],
       [1, 0, 0, 0, 0, 0],
       [1, 1, 0, 1, 1, 1],
-      [1, 1, 0, 0, 0, 0],
-      [1, 1, 0, 1, 1, 0]]
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 0]]
 
 maze = listArray ((1,1), (6, 6)) (concat mz)
 
 test = solveMaze maze (1,1) (6,6)
 
 test2 = dfsSolve maze (1,1) (6,6)
+
+test3 = dfsSolve maze (1,1) (6,6)
