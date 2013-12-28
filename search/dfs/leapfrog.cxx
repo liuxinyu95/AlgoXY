336ac82fb583f7e65acfcec6af399c6e1cad8cb2
liuxinyu95 <liuxinyu95@gmail.com>
2013-12-28 09:16:22 +0800

    Went on writing c++ leap frog puzzle solution

--- search/dfs/leapfrog.cxx
+++ search/dfs/leapfrog.cxx
@@ -1,23 +1,55 @@
 #include <list>
+#include <vector>
+#include <algorithm>
 #include <cstdio>
 
-#using namespace std;
+using namespace std;
 
-typedef list<int> State;
+typedef vector<int> State;
 typedef list<State> Steps;
 typedef list<Steps> Moves;
 
+Steps pop(Moves& stack) {
+    Steps top = stack.front();
+    stack.pop_front();
+    return top;
+}
+
+Steps append(Steps steps, State s) {
+    steps.push_back(s);
+    return steps;
+}
+
+State swap(State s, int i, int j) {
+    swap(s[i], s[j]);
+    return s;
+}
+
+list<State> moves(State s) {
+    list<State> ms;
+    int n = s.size(), p = find(s.begin(), s.end(), 0) - s.begin();
+    if (p < n - 2 && s[p+2] > 0)  /* leap left */
+        ms.push_back(swap(s, p, p+2));
+    if (p < n - 1 & s[p+1] > 0)   /* hop left */
+        ms.push_back(swap(s, p, p+1));
+    if (p > 1 && s[p-2] < 0)     /* leap right*/
+        ms.push_back(swap(s, p, p-2));
+    if (p > 0 && s[p-1] < 0)     /* hop right */
+        ms.push_back(swap(s, p, p-1));
+    return ms;
+}
+
 list<State> solve(State start, State end) {
-    Moves s, stack(1, start);
-    Steps cur, attemps;
+    Moves s, stack(1, Steps(1, start));
     while (!stack.empty()) {
-        if (stack.front().back() == end) {
-            s.push_back(stack.front());
-            stack.pop_front();
-        } else {
-            Steps cur = stack.front();
-            stack.
-            cur.back();
+        Steps cur = pop(stack);
+        if (cur.back() == end)
+            s.push_back(cur);
+        else {
+            list<State> attempts = moves(cur.back());
+            list<State>::iterator it;
+            for (it = attempts.begin(); it!=attempts.end(); ++it)
+                stack.push_front(append(cur, *it));
         }
     }
     return s;
