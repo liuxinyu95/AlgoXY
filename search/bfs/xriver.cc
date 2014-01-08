895006e74f4a74568f69c33c852de6e5e2451a60
Larry Liu <larry.1.liu@nokia.com>
2014-01-08 15:12:40 +0800

    Drafted xriver.c, not done

--- search/bfs/xriver.cc
+++ search/bfs/xriver.cc
@@ -1,5 +1,7 @@
 #include <queue>
 #include <list>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,14 +20,33 @@ Steps append(Steps steps, unsigned s) {
     return steps;
 }
 
-Steps moves(Steps s) {
+bool invalid(const Steps& s, int x) {
+    return (x & 3) || (x & 6) || (x & (3 << 4)) || (x & (6 << 4)) || 
+        find(s.begin(), s.end(), x) != s.end();
+}
+
+unsigned state(unsigned a, unsigned b) {
+    return (a << 4) + b;
+}
+
+Steps moves(const Steps& s) {
     unsigned a = s.back() & 0xf0, b = s.back() & 0x0f;
-    
+    Steps ms;
+    int i, mask, x;
+    for (i = 0; i < 4; ++i) {
+        mask = 8 & (1 << i);
+        if ((a & mask) && (!invalid(s, x = state(a ^ mask, b | mask))))
+            ms.push_back(x);
+        if ((b & mask) && (!invalid(s, x = state(a | mask, b ^ mask))))
+            ms.push_back(x);
+    }
+    return ms;
 }
 
 Solution solve() {
     Solution s;
-    Queue q(Steps(1, 0xf0));
+    Queue q;
+    q.push(Steps(1, 0xf0));
     while (!q.empty()) {
         Steps cur = pop(q);
         if (cur.back() == 0x0f)
@@ -37,4 +58,37 @@ Solution solve() {
         }
     }
     return s;
+}
+
+void print_set(unsigned s) {
+    int i;
+    const char* ns[] = {"wolf", "goat", "cabbage", "farmer"};
+    cout<<"[";
+    for (i = 0; i < 4; ++i)
+        if (s & (1 << i))
+            cout<<ns[i]<<" ";
+    cout<<"]";
+}
+
+void print_move(unsigned x) {
+    print_set(x & 0xf0);
+    cout<<"====";
+    print_set(x & 0xf);
+    cout<<"\n";
+}
+
+void test() {
+    Steps::iterator it;
+    Solution s = solve();
+    for(Solution::iterator is = s.begin(); is != s.end(); ++is) {
+        for (Steps::iterator it = is->begin(); it != is->end(); ++it)
+            print_move(*it);
+        cout<<"total "<<is->size()<<" steps.\n\n";
+    }
+    cout<<"total "<<s.size()<<" solutions.\n";
+}
+
+int main(int, char**) {
+    test();
+    return 0;
 }
