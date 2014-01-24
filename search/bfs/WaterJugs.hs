87ba35122e0c90939d3adac1f1be6de47b6ea68c
Larry Liu <larry.1.liu@nokia.com>
2014-01-24 15:29:36 +0800

    Fixed problem in ext-gcd solution; Provided bfs solution

--- search/bfs/WaterJugs.hs
+++ search/bfs/WaterJugs.hs
@@ -1,16 +1,16 @@
 -- WaterJugs.hs
 -- Copyright (C) 2014 Liu Xinyu (liuxinyu95@gmail.com)
--- 
+--
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
--- 
+--
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -81,9 +81,26 @@ solve a b g | g `mod` d /= 0 = [] -- no solution
       (d, x, y) = extGcd a b
       solve' x | x < 0 = solve' (x + b)
                | otherwise = pour x [(0, 0)]
-      pour 0 ps = reverse ps
-      pour n ps@((a', b'):_) | a' == 0 = pour (n-1) ((a, b'):ps) -- fill a
-                             | b' == b = pour (n-1) ((a', 0):ps) -- empty b
-                             | otherwise = pour (n-1) ((max 0 (a' + b' - b), min (a' + b') b):ps)
+      pour 0 ps = reverse ((0, g):ps)
+      pour x ps@((a', b'):_) | a' == 0 = pour (x - 1) ((a, b'):ps) -- fill a
+                             | b' == b = pour x ((a', 0):ps) -- empty b
+                             | otherwise = pour x ((max 0 (a' + b' - b), min (a' + b') b):ps)
 
 -- Method 2, BFS (brute-force)
+--solve' :: (Integral a) => a -> a -> a -> [(a, a)]
+solve' a b g = bfs [[(0, 0)]] where
+  bfs [] = []
+  bfs (c:cs) | fst (head c) == g || snd (head c) == g = reverse c
+             | otherwise = bfs (cs ++ map (:c) (expand c))
+  expand ((x, y):ps) = filter (`notElem` ps) $ map (\f -> f x y)
+                           [fillA, fillB, pourA, pourB, emptyA, emptyB]
+  fillA _ y = (a, y)
+  fillB x _ = (x, b)
+  emptyA _ y = (0, y)
+  emptyB x _ = (x, 0)
+  pourA x y = (max 0 (x + y - b), min (x + y) b)
+  pourB x y = (min (x + y) a, max 0 (x + y - a))
+
+-- Some test data:
+-- a = 3, b = 5, g = 4
+-- a = 4, b = 9, g = 6
