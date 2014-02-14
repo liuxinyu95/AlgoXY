b76f2923ab7f4788b96474e450b0d91116dc4799
Larry Liu <larry.1.liu@nokia.com>
2014-02-14 15:21:40 +0800

    Added some caution notes

--- search/bfs/Klotski.hs
+++ search/bfs/Klotski.hs
@@ -1,16 +1,16 @@
 -- Klotski.hs
 -- Copyright (C) 2014 Liu Xinyu (liuxinyu95@gmail.com)
--- 
+--
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
--- 
+--
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -19,7 +19,7 @@ module Klotski where
 import qualified Data.Map as M
 import Data.Ix
 import Data.List (sort)
-import Data.Array         -- for pretty output purpose only
+import Data.Array
 import Numeric (showHex)  -- for pretty output purpose only
 
 -- `Heng Dao Li Ma' Layout
@@ -79,6 +79,9 @@ shift (y, x) (dy, dx) = (y + dy, x + dx)
 klotski = let x = M.fromList start in solve [Op x []] [layout x]
 
 -- the followings are for pretty print only.
+
+-- !!! Note that this program is a bit SLOW, it takes minutes typically, please wait a
+-- while for the result being printed.
 
 toTable = toTab . map (flip showHex "") . elems . toArray where
   toTab [] = []
