f6792825f9faf85dad70ee6a3371de8f90473da5
Larry LIU Xinyu <liuxinyu95@gmail.com>
2014-04-11 10:33:20 +0800

    Added functional change making solution

--- search/greedy/ChangeMk.hs
+++ search/greedy/ChangeMk.hs
@@ -1,16 +1,16 @@
 -- ChangeMk.hs
 -- Copyright (C) 2014 Liu Xinyu (liuxinyu95@gmail.com)
--- 
+--
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
--- 
+--
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,7 +22,7 @@ import Data.List (group)
 
 solve x = assoc . change x where
   change 0 _ = []
-  change x cs = let c = head $ filter (<= x) cs in c : (change (x - c) cs)
+  change x cs = let c = head $ filter (<= x) cs in c : change (x - c) cs
 
 assoc = (map (\cs -> (head cs, length cs))) . group
 
