6403ebc57ecea727cfc4804ae96a595b5cdb1685
Larry LIU Xinyu <liuxinyu95@gmail.com>
2014-04-18 11:34:34 +0800

    Added another example to python top-down change-making with table solution

--- search/dp/changemk.py
+++ search/dp/changemk.py
@@ -2,17 +2,17 @@
 
 # changemk.py
 # Copyright (C) 2014 Liu Xinyu (liuxinyu95@gmail.com)
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -32,6 +32,9 @@ def change(x, cs):
 def test():
     USA = [1, 5, 25, 50, 100]
     print(change(142, USA))
+    for i in range(1000):
+        tab[i] = []
+    print(change(6, [1, 2, 4]))
 
 if __name__ == "__main__":
     test()
