b36ff98c6d90ceb22193fab0ea4619b8794d449b
Larry LIU Xinyu <liuxinyu95@gmail.com>
2014-05-08 15:30:50 +0800

    Started DP solutoin to subset sum problem

--- others/problems/subset-sum/subsetsum.py
+++ others/problems/subset-sum/subsetsum.py
@@ -2,17 +2,17 @@
 
 # subsetsum.py
 # Copyright (C) 2014 Liu Xinyu (liuxinyu95@gmail.com)
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -20,6 +20,7 @@
 import random
 
 # DP solution based on [1].
+# Note the following solution only answers the existence of subset for a given value.
 
 class qtab:
     def __init__(self, n, low, up):
