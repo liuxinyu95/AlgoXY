9c707a6215a31bc4db49d2426931fcd4de4b2425
Larry Liu <larry.1.liu@microsoft.com>
2014-09-10 15:43:13 +0800

    [circular list] Added the purely functional brent's solution

--- others/appendix/list/problems/circular/CDetect.hs
+++ others/appendix/list/problems/circular/CDetect.hs
@@ -25,16 +25,31 @@ findCycle' x0 f = (length sec, length cycle) where
   (sec, (z:zs)) = span neq (zip xs converge)
   cycle = z : takeWhile (z /=) zs
 
+-- Mehod 2, Richard P. Brent's algorithm
+detectCycle x0 f = (k, n) where
+  n = converge x0 (f x0) 1 1
+  q = foldr ($) x0 (replicate n f)
+  k = connect x0 q
+  connect p q | p == q = 0
+              | otherwise = 1 + connect (f p) (f q)
+  converge p q n m | n == m = converge q (f q) 1 (2*m)
+                   | p == q = n
+                   | otherwise = converge p (f q) (n + 1) m
+
 -- Verification
+
+-- Specified function f on non-negative integers.
 f k m n | n < k = n + 1
         | otherwise = (n + 1 - k) `mod` m + k
 
--- limit the k, m < 1000 to save the running time
-norm k m = ((abs k) `mod` 1000, max 1 (abs m) `mod` 1000)
+prop_cycle fcycle b c = (k, m) == fcycle 0 (f k m) where
+  (k, m) = ((abs b) `mod` 1000, max 1 (abs c) `mod` 1000)   -- limit to 1000 to save the time
 
 prop_floyd :: Int -> Int -> Bool
-prop_floyd b c = (k, m) == findCycle 0 (f k m) where
-  (k, m) = norm b c
+prop_floyd = prop_cycle findCycle
 
 prop_floyd' :: Int -> Int -> Bool
-prop_floyd' b c = (k, m) == findCycle' 0 (f k m) where (k, m) = norm b c
+prop_floyd' = prop_cycle findCycle'
+
+prop_brent :: Int -> Int -> Bool
+prop_brent = prop_cycle detectCycle
