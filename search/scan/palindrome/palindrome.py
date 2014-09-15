779b8feb1f0631b3454603f3d78349b399db4a03
Larry Liu <larry.1.liu@microsoft.com>
2014-09-15 10:37:13 +0800

    [palindrome] Fixed the mistake in brute-force algorithm

--- search/scan/palindrome/palindrome.py
+++ search/scan/palindrome/palindrome.py
@@ -1,16 +1,16 @@
 # palindrome.py
 # Copyright (C) 2014 Liu Xinyu (liuxinyu95@gmail.com)
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -23,7 +23,11 @@ def brute_force_palindrome(s):
     n = len(s)
     p = [0] * n
     for i in xrange(n):
-        for k in xrange(n):
-            while 0 <= i-k and i+k < n and s[i-k] == s[i+k]:
-                p[i] = p[i] + 1
-    return max(p)
+        k = 0
+        while 0 <= i-k and i+k < n and s[i-k] == s[i+k]:
+            p[i] = p[i] + 1
+            k = k + 1
+    return max(p) - 1
+
+if __name__ == "__main__":
+    print brute_force_palindrome("Mississippi")
