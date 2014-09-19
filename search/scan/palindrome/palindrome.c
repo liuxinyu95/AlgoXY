99c6f4165e0bbd857facec3df9f00e900138db96
Larry Liu <larry.1.liu@microsoft.com>
2014-09-19 17:06:19 +0800

    [palindrome] Have access vialoation issue in online-judge

--- search/scan/palindrome/palindrome.c
+++ search/scan/palindrome/palindrome.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 #define DELIMITER '#'
-#define MAX_LEN 11000
+#define MAX_LEN 11005
 
-char s[MAX_LEN * 2 + 5];
-unsigned p[MAX_LEN * 2 + 5];
+char s[MAX_LEN * 2];
+unsigned p[MAX_LEN * 2];
 
 int min(int a, int b) {
     return a < b ? a : b;
 }
 
-unsigned manacher_palindrome(char* str) {
-    unsigned i, j, m, n = strlen(str);
-    for (i = 0; i <= n; ++i) {
+int manacher_palindrome(char* s) {
+    int i, j, m, n = strlen(s);
+    for (i = n; i >= 0; --i) {
+        s[2*i + 1] = s[i];
         s[2*i] = DELIMITER;
-        s[2*i + 1] = str[i];
     }
-    for (i = j = m = p[0] = 1, n = 2*n + 1; i < n; ++i) {
+    for (i = j = m = p[0] = 0, n = 2*n + 1; i < n; ++i) {
         p[i] = i < j + p[j]? min(p[2*j - i], j + p[j] - i) : 1;
         while (p[i] <= i && i + p[i] < n && s[i-p[i]] == s[i+p[i]])
             ++p[i];
@@ -30,11 +30,19 @@ unsigned manacher_palindrome(char* str) {
 void test() {
     char* ss[] = {"Mississippi", "level", "cacab", "cocoa", "aaa", "abc"};
     int i;
-    for (i = 0; i < sizeof(ss)/sizeof(ss[0]); ++i)
-        printf("longest palindrome for %s, len=%u\n", ss[i], manacher_palindrome(ss[i]));
+    for (i = 0; i < sizeof(ss)/sizeof(ss[0]); ++i) {
+        strcpy(s, ss[i]);
+        printf("longest palindrome for %s, len=%d\n", ss[i], manacher_palindrome(s));
+    }
+}
+
+void online_judge() {
+    while (scanf("%s", s) != EOF)
+        if (strlen(s)) printf("%d\n", manacher_palindrome(s));
 }
 
 int main(int argc, char** argv) {
-    test();
+    online_judge();
+    /*test();*/
     return 0;
 }
