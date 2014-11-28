656074e49f2e9962c99ad30e765e0e6aff1446d4
liuxinyu95 <liuxinyu95@gmail.com>
2014-11-29 07:52:19 +0800

    [Conway slide puzzle] Solution works

--- others/problems/search/BFS/conway-slide/ConwaySlide.hs
+++ others/problems/search/BFS/conway-slide/ConwaySlide.hs
@@ -1,16 +1,14 @@
 module ConwaySlide where
 
-import Debug.Trace
-
 solve [] _ = [] -- no slution
 solve (q@(n:ns):qs) h | n == [0, 7, 6, 5, 4, 3, 2, 1] = reverse q
-                      | otherwise = let (cs, h') = (slide q (trace ("q: " ++ show q) h)) in solve (qs ++ cs) h'
+                      | otherwise = let (cs, h') = (slide q h) in solve (qs ++ cs) h'
 
 slide q@(n:_) h = (map (:q) ns, ns ++ h) where
   ns = [n' | n' <- map ($ n) [left, right, up, down], n' /= [], n' `notElem` h]
 
 -- xxxa0xxx -> xxx0axxx
-right (0:xs) = xs ++ [0]
+right (0:xs) = (last xs):(init xs) ++ [0]
 right xs = let (as, (_:bs)) = break (==0) xs in (init as) ++ (0 : last as : bs)
 
 -- xxx0axxx -> xxxa0xxx
@@ -24,4 +22,4 @@ down _ = []
 up (a:b:c:d:0:xs) = 0:b:c:d:a:xs
 up _ = []
 
-ans = solve [[[0 .. 7]]] []
+ans = solve [[[0..7]]] [[0..7]]
