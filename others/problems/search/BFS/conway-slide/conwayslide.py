217b6c122c264f25cb71a9cdb5a6c7ffd190b5d6
liuxinyu95 <liuxinyu95@gmail.com>
2014-11-30 16:21:38 +0800

    [Conway slide puzzle] Added python solution

---
+++ others/problems/search/BFS/conway-slide/conwayslide.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+from collections import deque
+
+def solve():
+    s0 = range(8)
+    q = deque([(s0, None)])
+    visit = {tuple(s0)}
+    while q:
+        s = q.pop()
+        (n, _) = s
+        if n == [0, 7, 6, 5, 4, 3, 2, 1]:
+            return backtrack(s)
+        else:
+            q.extendleft(slide(s, visit))
+    return None # no solution
+
+def backtrack(s):
+    r = []
+    (n, p) = s
+    while p is not None:
+        r.append(n)
+        (n, p) = p
+    r.append(n)
+    return r[::-1]
+
+def slide(s, visit):
+    (n, _) = s
+    cs = []
+    for i in [left(n), right(n), up(n), down(n)]:
+        if i != [] and (tuple(i) not in visit):
+            visit.add(tuple(i))
+            cs.append((i, s))
+    return cs
+
+def right(n):
+    m = n[:]
+    i = m.index(0)
+    (m[i], m[i-1]) = (m[i-1], m[i])
+    return m
+
+def left(n):
+    m = n[:]
+    i = m.index(0)
+    (m[i], m[i-7]) = (m[i-7], m[i])
+    return m
+
+def up(n):
+    return ([0] + n[1:4] + [n[0]] + n[5:]) if n[4] == 0 else []
+
+def down(n):
+    return ([n[4]] + n[1:4] + [0] + n[5:]) if n[0] == 0 else []
+
+if __name__ == "__main__":
+    print(solve())
