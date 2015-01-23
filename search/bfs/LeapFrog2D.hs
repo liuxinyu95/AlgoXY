747dbb1df7deed5169a19071333265614a8efa39
Larry Liu <larry.1.liu@microsoft.com>
2015-01-23 05:00:42 +0200

    Fixed bugs in bit ops

--- search/bfs/LeapFrog2D.hs
+++ search/bfs/LeapFrog2D.hs
@@ -19,59 +19,66 @@ type History = M.Map Layout Layout
 
 traceLog msg a = trace (msg ++ (show a)) a
 
+start = bin $ concat [[1,1,1,0,0],
+                      [1,1,1,0,0],
+                      [1,1,0,0,0],
+                      [0,0,0,0,0],
+                      [0,0,0,0,0]]
+
+end = bin $ concat [[0,0,0,0,0],
+                    [0,0,0,0,0],
+                    [0,0,0,1,1],
+                    [0,0,1,1,1],
+                    [0,0,1,1,1]]
+
+s0 = ((3, 3), start)
+
 ans = solve [s0] (M.singleton s0 s0) where
   solve :: [Layout] -> History -> [Layout]
   solve [] _ = []
-  solve (c@((i,j), n):cs) h | n == end = backtrack c h []
-                            | otherwise = let (cs', h') = move (i, j) n h in solve (cs ++ (traceLog "cs'=" cs')) h'
-s0 = ((3, 3), start)
-
-start = let r = [1,1,1,0,0] in norm $ bin $ concat [r, r, r, reverse r, reverse r]
-end = norm (complement start)
+  solve (c@(p, n):cs) h | n == end = backtrack c h []
+                        | otherwise = let (cs', h') = move p n h in solve (cs ++ (traceLog "cs'=" cs')) h'
 
 move :: Point -> Integer -> History -> ([Layout], History)
 move p n h = (cs, foldr (\c h' -> M.insert c (p, n) h') h cs) where
   delta = concat [[(di, dj), (2*di, 2*dj)] | (di, dj) <- [(0, 1), (1, 0), (0, -1), (-1, 0)]]
   cs = [(p', n') | d <- delta, let p' = p `offset` d,
-        inBoard p', getAt n p' == signum (fst d + snd d),
-        let n' = norm $ swapbits n p p', (p', n') `M.notMember` h]
+        inBoard p', getAt n p' == (- signum (fst d + snd d)),
+        let n' = norm p' $ swapbits n p p', (p', n') `M.notMember` h]
 
 backtrack c h cs = let c' = h M.! c in if c' == c then cs else backtrack c' h (c:cs)
 
 -- bitwise functions
 
-bin = foldr1 (\d n -> d + 2*n)
+bin = foldl1 (\n d -> 2*n + d)
 
 toInt :: Integer -> Int
 toInt = fromIntegral
 
-getbit n i = toInt ((n `shiftR` (i-1)) .&. 1)
+getbit n i = toInt ((n `shiftR` i) .&. 1)
 setbit n i = n .|. (1 `shiftL` i)
 clrbit n i = n .&. (complement (1 `shiftL` i))
 
 swapbits :: Integer -> Point -> Point -> Integer
-swapbits n p p' = setAt (setAt n d p') d' p where
-  d = getbit n (idx p)
-  d' = getbit n (idx p')
+swapbits n p p' = setAt (setAt n d m') d' m
+  where
+    (m, m') = (idx p, idx p')
+    (d, d') = (getbit n m, getbit n m')
+
+setAt n 1 m = setbit n m
+setAt n 0 m = clrbit n m
 
 getAt :: Integer -> Point -> Int
 getAt n p = 2 * (getbit n (idx p)) - 1
 
-setAt :: Integer -> Int -> Point -> Integer
-setAt n 1 p = setbit n (idx p)
-setAt n 0 p = clrbit n (idx p)
-
 -- board related
 
 idx :: Point -> Int
-idx (i, j) = 5 * (i-1) + j - 1
+idx (i, j) = 25 - 5 * (i-1) - j
 
-mask :: Integer
-mask = foldl setbit 0 (map idx [(1, 4), (1, 5), (2, 4), (2, 5), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2)])
+norm p n = clrbit n (idx p)
 
-norm = (mask .&.)
-
-offset (x, y) (dx, dy) = (x+dx, y+dy)
+offset (x, y) (dx, dy) = (x + dx, y + dy)
 
 inBoard p = inRange s1 p || inRange s2 p where
   s1 = ((1, 1), (3, 3))
