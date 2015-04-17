430634d1b037428a77376f3584b9100aef0eb448
Larry Liu <larry.1.liu@microsoft.com>
2015-04-17 11:38:28 +0800

    Removed the in-accurate statement

--- others/problems/search/divide-conquer/median.py
+++ others/problems/search/divide-conquer/median.py
@@ -14,13 +14,6 @@
 #   Otherwise, m2 < m1:
 #      Recursive find the median of two sub-arrays: A[...m1] and B[m2...]
 
-# Edge termination case
-#   Both sub-arrays contain 2 elements foreach.
-#     return (max(a1, b1) + min(a2, b2)) / 2
-
-# Other handling:
-#   Either array contains less than 2 elements or empty.
-
 def median(xs, ys):
     if not xs:
         return med(ys)
