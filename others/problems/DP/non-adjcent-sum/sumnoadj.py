9c9952c12cbc013155dbfa1f19b2c0990afb4954
Larry Liu <larry.1.liu@microsoft.com>
2015-04-22 10:30:35 +0800

    Working max non-adjacent sum solution

--- others/problems/DP/non-adjcent-sum/sumnoadj.py
+++ others/problems/DP/non-adjcent-sum/sumnoadj.py
@@ -2,41 +2,53 @@
 
 # maximum sum of non-adjacent numbers.
 
+# solution 1:
+# Scan the list in one round, keep two variables i, e:
+#   i: The maximum sum so far including current element
+#   e: The maximum sum so far excluding current element
+# Optimal sub structure
+#   Scan the list from beginning, for any element xs[i], update i and e:
+#   i' = xs[i] + e    include this element, and exclude the previous one;
+#   e' = max(e, i)    skip this one, the maxium of previous i and e.
 def maxsum(xs):
-    if xs == []:
-        return 0
-    low = max(xs)
+    i, e = 0, 0
+    for x in xs:
+        i, e = x + e, max(i, e)
+    return max(i, e)
+
+# solution 2: sub-set sum like DP solution
+def maxsum2(xs):
+    n = len(xs)
+    if n <= 2:
+        return 0 if not xs else max(xs)
+    low = min(xs)
     up = sum(xs)
-    print "low=", low, "up=", up, "xs=", xs
-    t = [[] for _ in xrange(up - low)]
-    print "t=", t
-    for i in xrange(len(xs)):
-        print "i=", i, "xs[i]=", xs[i]
-        t[xs[i] - low] = [[i]]
-    for i in xrange(len(xs)):
-        tab = t[:]
+    if low == up:
+        return low
+    tab = [-1 for _ in xrange(up - low)] #-1: empty, else: the last selected index
+    for i in xrange(n):
+        tab[xs[i] - low] = i
+    for i in xrange(n):
         for v in xrange(up - low):
             v1 = v - xs[i]
-            if 0 <= v1 and v1 < up - low and tab[v1] != []:
-                tab[v] = tab[v] + [[i] + ys for ys in tab[v1] if not adjacent(i, tab[v1])]
-        t = tab
+            if 0 <= v1 and v1 < up - low and tab[v1] not in [-1, i, i-1, i+1]:
+                tab[v] = i
     for v in reversed(xrange(up - low)):
-        if tab[v] != []:
-            return sum([xs[i] for i in tab[v]])
-    return 0 #shouldn't be here
-
-def adjacent(i, ns):
-    return i-1 in ns or (i+1) in ns
+        if tab[v] != -1:
+            return v + low
+    return 0
 
 def test():
-    check([], 0)
-    check([1], 1)
-    check([1, 2], 2)
-    check([1, 2, 3], 4)
-    check([1, 2, 3, 4], 6)
+    check([])
+    check([1])
+    check([1, 2])
+    check([1, 2, 3])
+    check([1, 2, 3, 4])
+    check([0, 0, 0])
+    check([1, 1, 1])
 
-def check(xs, s):
-    s1 = maxsum(xs)
+def check(xs):
+    s, s1 = maxsum(xs), maxsum2(xs)
     if s1 != s:
         print "FAIL: expected:", s, "actual:", s1
         print xs
