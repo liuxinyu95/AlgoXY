4efd69835b7f5ba9d7f33245722909e4d647b4c4
Larry Liu <larry.1.liu@microsoft.com>
2015-04-23 11:00:02 +0800

    Added lazy infinite solution for max sum of non-adj elems

--- others/problems/DP/non-adjcent-sum/MaxSum.hs
+++ others/problems/DP/non-adjcent-sum/MaxSum.hs
@@ -2,6 +2,7 @@
 
 module MaxSum where
 
+import Data.List (zipWith3)
 import Test.QuickCheck
 
 -- top-down recursive method
@@ -14,5 +15,15 @@ maxsum (x:y:xs) = max (x + maxsum xs) (maxsum (y:xs))
 maxsum2 xs = max a b where
   (a, b) = foldl (\(i, e) x -> (x + e, max i e)) (0, 0) xs
 
+-- lazy infinite recursive definition similiar to Fibonacci
+maxsums [] = [0]
+maxsums [x] = [0, x]
+maxsums [x, y] = [0, x, max x y]
+maxsums (x:y:xs) = ms where
+  ms = 0 : x : (max x y) : zipWith3 (\x' m m' -> max (x' + m) m') xs (tail ms) (drop 2 ms)
+
 prop_maxsum :: [Int] -> Bool
 prop_maxsum xs = let ns = take 20 $ map (abs . (`mod` 100)) xs in maxsum ns == maxsum2 ns
+
+prop_maxsums :: [Int] -> Bool
+prop_maxsums xs = let ns = map (abs . (`mod` 100)) xs in maxsum2 ns == (last $ maxsums ns)
