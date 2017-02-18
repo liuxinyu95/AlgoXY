207d613a5abf22318067ea942ad2ea94c10ea471
Liu Xinyu <xinyul@amazon.com>
2017-02-18 21:02:30 +0800

    Added comments to the scala miss-dup example

--- others/problems/miss-dup/MissDup.scala
+++ others/problems/miss-dup/MissDup.scala
@@ -1,4 +1,17 @@
+/*
+ * Given numbers from 1 to n, after some processing, there are some changes.
+ *  1) The order is shuffled;
+ *  2) One number x is mutated to y, here both x, y are from 1 to n.
+ * Develop a method to find the x and y in linear time with constant space.
+ *
+ * Examples
+ * [3, 1, 3, 5, 4] ==> x = 2, y = 3
+ */
+
 object MissDup {
+
+  // divide and conquer
+
   def missDup(xs : List[Int]) : (Int, Int) = {
     def solve(xs : List[Int], l : Int, u : Int) : (Int, Int) = {
       val m = (l + u) / 2
@@ -21,6 +34,8 @@ object MissDup {
     }
     solve(xs, 1, xs length)
   }
+
+  // verification
 
   def test() {
     val r = scala.util.Random
