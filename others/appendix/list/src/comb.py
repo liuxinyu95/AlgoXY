6cd279850fbe6c83586d4e05a34bad2d5744dcaf
Liu Xinyu <xinyul@amazon.com>
2017-05-17 08:32:44 +0800

    Added another recursive method

--- others/appendix/list/src/comb.py
+++ others/appendix/list/src/comb.py
@@ -14,10 +14,23 @@ def comb(xs, r):
     dfs(0, [])
     return acc
 
+# Another recursive solution
+def combination(xs, r):
+   if r <= 0 or r > len(xs):
+       return []
+   if (r == 1):
+       return [[x] for x in xs]
+   res = []
+   for i in range(len(xs)):
+       yss = combination(xs[i + 1 : ], r - 1)
+       res = res + [[xs[i]] + ys for ys in yss]
+   return res
+
 def test():
     xs = ['a', 'b', 'c', 'd']
     for r in range(6):
         print len(xs), "choose", r, ":", comb(xs, r)
+        print len(xs), "choose", r, ":", combination(xs, r)
 
 if __name__ == "__main__":
     test()
