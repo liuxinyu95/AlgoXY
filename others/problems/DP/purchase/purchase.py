63186f69fbd1d63d9abc4ed09990a362c88c6468
Xinyu Liu <liuxinyu95@gmail.com>
2019-10-27 14:00:22 +0800

    optimized dp table update

--- others/problems/DP/purchase/purchase.py
+++ others/problems/DP/purchase/purchase.py
@@ -1,16 +1,16 @@
 # purchase.py
 # Copyright (C) 2019 Liu Xinyu (liuxinyu95@gmail.com)
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -54,9 +54,9 @@ def dp(plans):
     for plan, (prods, price) in plans.items():
         for cost, (plan_set, prod_set) in tab.items():
             _cost = price + cost
-            _prod_set = prod_set.union(prods)
+            _prod_set = set(prods)
             if _cost not in tab or not _prod_set.issubset(prod_set):
-                tab[_cost] = (plan_set.union([plan]), _prod_set)
+                tab[_cost] = (plan_set.union([plan]), _prod_set.union(prod_set))
     return tab
 
 def lowest(prods, tab):
