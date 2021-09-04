73a6d6e6c5adf8ccef11ca10a4d371fb016aa714
Xinyu LIU <liuxinyu95@gmail.com>
2021-09-04 19:58:33 +0800

    BTree: 1, skip duplicated key in insert; 2, QuickCheck BTree property

--- datastruct/tree/B-tree/src/BTree.hs
+++ datastruct/tree/B-tree/src/BTree.hs
@@ -19,137 +19,131 @@
 module BTree where
 
 import qualified Data.List as L
-import Control.Monad (foldM_, mapM_)
+import Test.QuickCheck
 
--- Definition
 data BTree a = BTree { keys :: [a]
-                     , subTrees :: [BTree a]
-                     , degree :: Int } deriving (Eq, Show)
+                     , subTrees :: [BTree a] } deriving (Eq, Show)
 
-empty t = BTree [] [] t
+empty = BTree [] []
 
-full (BTree ks _ t) = (length ks) > 2 * t - 1
+full d (BTree ks _) = (length ks) > 2 * d - 1
 
-low (BTree ks _ t) = (length ks) < t - 1
+low d (BTree ks _) = (length ks) < d - 1
 
-insert :: (Ord a) => BTree a -> a -> BTree a
-insert tr x = fixRoot $ ins tr x
+insert :: (Ord a) => a -> (Int, BTree a) -> (Int, BTree a)
+insert x (d, t) = fixRoot (d, ins t) where
+    ins (BTree ks ts) | x `elem` ks = (BTree ks ts) -- skip duplicated key
+                      | null ts = BTree (L.insert x ks) []
+                      | otherwise = balance d (ks1, ts1) (ins t') (ks2, ts2) where
+      (ks1, ks2) = L.partition (< x) ks
+      (ts1, (t':ts2)) = L.splitAt (length ks1) ts
 
-ins :: (Ord a) => BTree a -> a -> BTree a
-ins (BTree ks [] t) x = BTree (L.insert x ks) [] t
-ins (BTree ks cs t) x = make (ks', cs') (ins c x) (ks'', cs'')
-    where
-      (ks', ks'') = L.partition (<x) ks
-      (cs', (c:cs'')) = L.splitAt (length ks') cs
+-- delete :: (Ord a) => BTree a -> a -> BTree a
+-- delete tr x = fixRoot $ del tr x
 
-delete :: (Ord a) => BTree a -> a -> BTree a
-delete tr x = fixRoot $ del tr x
+-- del:: (Ord a) => BTree a -> a -> BTree a
+-- del (BTree ks [] t) x = BTree (L.delete x ks) [] t
+-- del (BTree ks cs t) x =
+--     case L.elemIndex x ks of
+--       Just i -> merge (BTree (take i ks) (take (i+1) cs) t)
+--                       (BTree (drop (i+1) ks) (drop (i+1) cs) t)
+--       Nothing -> balance (ks', cs') (del c x) (ks'', cs'')
+--     where
+--       (ks', ks'') = L.partition (<x) ks
+--       (cs', (c:cs'')) = L.splitAt (length ks') cs
 
-del:: (Ord a) => BTree a -> a -> BTree a
-del (BTree ks [] t) x = BTree (L.delete x ks) [] t
-del (BTree ks cs t) x =
-    case L.elemIndex x ks of
-      Just i -> merge (BTree (take i ks) (take (i+1) cs) t)
-                      (BTree (drop (i+1) ks) (drop (i+1) cs) t)
-      Nothing -> make (ks', cs') (del c x) (ks'', cs'')
-    where
-      (ks', ks'') = L.partition (<x) ks
-      (cs', (c:cs'')) = L.splitAt (length ks') cs
+-- lookup :: (Ord a)=> a -> BTree a -> Maybe (BTree a, Int)
+-- lookup tr@(BTree ks cs _) k
+--     | matchFirst k $ drop len ks = Just (tr, len)
+--     | otherwise = if null cs then Nothing
+--                   else lookup (cs !! len) k
+--     where
+--       matchFirst x (y:_) = x==y
+--       matchFirst x _ = False
+--       len = length $ filter (<k) ks
 
--- Search
+split d (BTree ks ts) = (BTree ks1 ts1, k, BTree ks2 ts2) where
+  (ks1, k:ks2) = L.splitAt (d - 1) ks
+  (ts1, ts2) = L.splitAt d ts
 
-search :: (Ord a)=> BTree a -> a -> Maybe (BTree a, Int)
-search tr@(BTree ks cs _) k
-    | matchFirst k $ drop len ks = Just (tr, len)
-    | otherwise = if null cs then Nothing
-                  else search (cs !! len) k
-    where
-      matchFirst x (y:_) = x==y
-      matchFirst x _ = False
-      len = length $ filter (<k) ks
+unsplit (BTree ks1 ts1) k (BTree ks2 ts2) = BTree (ks1 ++ [k] ++ ks2)
+                                                  (ts1 ++ ts2)
 
--- Tree manipulation
+merge _ (BTree ks []) (BTree ks' [])  = BTree (ks ++ ks') []
+merge d (BTree ks ts) (BTree ks' ts') = balance d (ks, init ts)
+                                                (merge d (last ts) (head ts'))
+                                                (ks', tail ts')
 
-split :: BTree a -> (BTree a, a, BTree a)
-split (BTree ks cs t) = (c1, k, c2) where
-    c1 = BTree (take (t-1) ks) (take t cs) t
-    c2 = BTree (drop t ks) (drop t cs) t
-    k  = head (drop (t-1) ks)
+fixRoot (d, BTree [] [t]) = (d, t)
+fixRoot (d, t) | full d t  = let (t1, k, t2) = split d t in
+                               (d, BTree [k] [t1, t2])
+               | otherwise = (d, t)
 
-unsplit :: BTree a -> a -> BTree a -> BTree a
-unsplit c1 k c2 = BTree ((keys c1)++[k]++(keys c2))
-                       ((subTrees c1)++(subTrees c2)) (degree c1)
+balance :: Int -> ([a], [BTree a]) -> BTree a -> ([a], [BTree a]) -> BTree a
+balance d (ks1, ts1) t (ks2, ts2)
+    | full d t  = fixFull
+    | low  d t  = fixLow
+    | otherwise = BTree (ks1 ++ ks2) (ts1 ++ [t] ++ ts2)
+  where
+    fixFull = let (t1, k, t2) = split d t in
+                BTree (ks1 ++ [k] ++ ks2) (ts1 ++ [t1, t2] ++ ts2)
+    fixLow | not $ null ks1 = balance d (init ks1, init ts1)
+                                      (unsplit (last ts1) (last ks1) t)
+                                      (ks2, ts2)
+           | not $ null ks2 = balance d (ks1, ts1)
+                                      (unsplit t (head ks2) (head ts2))
+                                      (tail ks2, tail ts2)
+           | otherwise = t
 
--- merge two nodes into one--  k1, k2, ..., kn          k1', k2', ..., km'
---C1, C2, ..., Cn, C{n+1}  C1',C2', ..., Cm', C{m+1}
--- recursively merge C{n+1} and C1' until both nodes are leaves
-merge :: BTree a -> BTree a -> BTree a
-merge (BTree ks [] t) (BTree ks' [] _) = BTree (ks++ks') [] t
-merge (BTree ks cs t) (BTree ks' cs' _) = make (ks, init cs)
-                                             (merge (last cs) (head cs'))
-                                             (ks', tail cs')
 
---  Fixing B-tree balance property
-fixRoot :: BTree a -> BTree a
-fixRoot (BTree [] [tr] _) = tr -- shrink height
-fixRoot tr = if full tr then BTree [k] [c1, c2] (degree tr)
-             else tr
-    where
-      (c1, k, c2) = split tr
+fromList d xs = foldr insert (d, empty) xs
 
-make :: ([a], [BTree a]) -> BTree a -> ([a], [BTree a]) -> BTree a
-make (ks', cs') c (ks'', cs'')
-    | full c = fixFull (ks', cs') c (ks'', cs'')
-    | low c  = fixLow  (ks', cs') c (ks'', cs'')
-    | otherwise = BTree (ks'++ks'') (cs'++[c]++cs'') (degree c)
+toList (BTree ks []) = ks
+toList (BTree ks (t:ts)) = concat ((toList t) : (zipWith (\k tr -> k : toList tr) ks ts))
 
-fixFull :: ([a], [BTree a]) -> BTree a -> ([a], [BTree a]) -> BTree a
-fixFull (ks', cs') c (ks'', cs'') = BTree (ks'++[k]++ks'')
-                                         (cs'++[c1,c2]++cs'') (degree c)
-    where
-      (c1, k, c2) = split c
+-- Quick check
 
-fixLow :: ([a], [BTree a]) -> BTree a -> ([a], [BTree a]) -> BTree a
-fixLow (ks'@(_:_), cs') c (ks'', cs'') = make (init ks', init cs')
-                                              (unsplit (last cs') (last ks') c)
-                                              (ks'', cs'')
-fixLow (ks', cs') c (ks''@(_:_), cs'') = make (ks', cs')
-                                              (unsplit c (head ks'') (head cs''))
-                                              (tail ks'', tail cs'')
-fixLow _ c _ = c
+degOf [] = 2
+degOf (x:_) = x `mod` 5 + 2 -- limit degree: 2 <= d < 6 for simplicity
 
-toString :: (Show a)=>BTree a -> String
-toString (BTree ks [] _) = "("++(L.intercalate ", " (map show ks))++")"
-toString tr = "("++(toStr (keys tr) (subTrees tr))++")" where
-    toStr (k:ks) (c:cs) = (toString c)++", "++(show k)++", "++(toStr ks cs)
-    toStr [] [c] = toString c
+isBTree :: Int -> Int -> (BTree a) -> Bool
+isBTree d n t@(BTree _ ts) = (not $ full d t)
+                          && (n == 0 || (not $ low d t))
+                          && (and [isBTree d (n + 1) tr | tr <- ts])
 
-listToBTree::(Ord a)=>[a]->Int->BTree a
-listToBTree lst t = foldl insert (empty t) lst
+prop_order :: [Int] -> Bool
+prop_order xs = (L.sort $ L.nub xs) == (toList $ snd $ fromList d xs) where
+  d = degOf xs
 
---test
-testInsert = do
-  putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZ" 3
-  --testInsertV "GMPXACDEJKNORSTUVYZ" 3
-  putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZ" 2
-  --testInsertV "GMPXACDEJKNORSTUVYZ" 2
-  putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZBFHIQW" 3
+prop_insert :: [Int] -> Bool
+prop_insert xs = isBTree d 0 $ snd $ fromList d xs where d = degOf xs
 
--- verbose
-testInsertV lst t =
-  mapM_ (\x->putStrLn $ toString $ listToBTree x t) (tail $ L.reverse $ L.tails lst)
+testAll = do
+  quickCheck prop_order
+  quickCheck prop_insert
 
-testDelete = foldM_ delShow (listToBTree "GMPXACDEJKNORSTUVYZBFHIQW" 3) "EGAMU" where
-    delShow tr x = do
-      let tr' = delete tr x
-      putStrLn $ "delete "++(show x)
-      putStrLn $ toString tr'
-      return tr'
+-- testInsert = do
+--   putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZ" 3
+--   --testInsertV "GMPXACDEJKNORSTUVYZ" 3
+--   putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZ" 2
+--   --testInsertV "GMPXACDEJKNORSTUVYZ" 2
+--   putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZBFHIQW" 3
 
-testSearch = mapM_ (showSearch (listToBTree lst 3)) $ lst++"L"
-    where
-      showSearch tr x = do
-        case search tr x of
-          Just (_, i) -> putStrLn $ "found" ++ (show x)
-          Nothing -> putStrLn $ "not found" ++ (show x)
-      lst = "GMPXACDEJKNORSTUVYZBFHIQW"
+-- Verbose
+-- testInsertV lst t =
+--   mapM_ (\x->putStrLn $ toString $ listToBTree x t) (tail $ L.reverse $ L.tails lst)
+
+-- testDelete = foldM_ delShow (listToBTree "GMPXACDEJKNORSTUVYZBFHIQW" 3) "EGAMU" where
+--     delShow tr x = do
+--       let tr' = delete tr x
+--       putStrLn $ "delete "++(show x)
+--       putStrLn $ toString tr'
+--       return tr'
+
+-- testSearch = mapM_ (showSearch (listToBTree lst 3)) $ lst++"L"
+--     where
+--       showSearch tr x = do
+--         case search tr x of
+--           Just (_, i) -> putStrLn $ "found" ++ (show x)
+--           Nothing -> putStrLn $ "not found" ++ (show x)
+--       lst = "GMPXACDEJKNORSTUVYZBFHIQW"
