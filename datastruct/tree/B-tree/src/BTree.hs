8d7ae6a15774ab0a12727de255bbb8062536f753
Xinyu LIU <liuxinyu95@gmail.com>
2021-09-06 20:33:17 +0800

    Fixed BTree delete implementation

--- datastruct/tree/B-tree/src/BTree.hs
+++ datastruct/tree/B-tree/src/BTree.hs
@@ -18,6 +18,9 @@
 
 module BTree where
 
+-- Implement the B-Tree with List,
+--   2-passes insert/delete (insert/delete then fix)
+
 import qualified Data.List as L
 import Test.QuickCheck
 
@@ -26,41 +29,21 @@ data BTree a = BTree { keys :: [a]
 
 empty = BTree [] []
 
+size (BTree ks _) = length ks
+
+-- d - 1 <= |keys| <= 2 * d - 1
+
 full d (BTree ks _) = (length ks) > 2 * d - 1
 
 low d (BTree ks _) = (length ks) < d - 1
 
-insert :: (Ord a) => a -> (Int, BTree a) -> (Int, BTree a)
-insert x (d, t) = fixRoot (d, ins t) where
-    ins (BTree ks ts) | x `elem` ks = (BTree ks ts) -- skip duplicated key
-                      | null ts = BTree (L.insert x ks) []
-                      | otherwise = balance d (ks1, ts1) (ins t') (ks2, ts2) where
-      (ks1, ks2) = L.partition (< x) ks
-      (ts1, (t':ts2)) = L.splitAt (length ks1) ts
+partition x (BTree ks ts) = (l, t, r) where
+  l = (ks1, ts1)
+  r = (ks2, ts2)
+  (ks1, ks2) = L.partition (< x) ks
+  (ts1, (t:ts2)) = L.splitAt (length ks1) ts
 
--- delete :: (Ord a) => BTree a -> a -> BTree a
--- delete tr x = fixRoot $ del tr x
-
--- del:: (Ord a) => BTree a -> a -> BTree a
--- del (BTree ks [] t) x = BTree (L.delete x ks) [] t
--- del (BTree ks cs t) x =
---     case L.elemIndex x ks of
---       Just i -> merge (BTree (take i ks) (take (i+1) cs) t)
---                       (BTree (drop (i+1) ks) (drop (i+1) cs) t)
---       Nothing -> balance (ks', cs') (del c x) (ks'', cs'')
---     where
---       (ks', ks'') = L.partition (<x) ks
---       (cs', (c:cs'')) = L.splitAt (length ks') cs
-
--- lookup :: (Ord a)=> a -> BTree a -> Maybe (BTree a, Int)
--- lookup tr@(BTree ks cs _) k
---     | matchFirst k $ drop len ks = Just (tr, len)
---     | otherwise = if null cs then Nothing
---                   else lookup (cs !! len) k
---     where
---       matchFirst x (y:_) = x==y
---       matchFirst x _ = False
---       len = length $ filter (<k) ks
+divide t = split ((size t) `div` 2) t
 
 split d (BTree ks ts) = (BTree ks1 ts1, k, BTree ks2 ts2) where
   (ks1, k:ks2) = L.splitAt (d - 1) ks
@@ -69,10 +52,32 @@ split d (BTree ks ts) = (BTree ks1 ts1, k, BTree ks2 ts2) where
 unsplit (BTree ks1 ts1) k (BTree ks2 ts2) = BTree (ks1 ++ [k] ++ ks2)
                                                   (ts1 ++ ts2)
 
-merge _ (BTree ks []) (BTree ks' [])  = BTree (ks ++ ks') []
-merge d (BTree ks ts) (BTree ks' ts') = balance d (ks, init ts)
-                                                (merge d (last ts) (head ts'))
-                                                (ks', tail ts')
+max' (BTree ks []) = last ks
+max' (BTree _  ts) = max' $ last ts
+
+insert :: (Ord a) => a -> (Int, BTree a) -> (Int, BTree a)
+insert x (d, t) = fixRoot (d, ins t) where
+    ins t@(BTree ks ts)
+      | x `elem` ks = t -- skip duplicated key
+      | null ts = BTree (L.insert x ks) []
+      | otherwise = balance d l (ins t') r where
+          (l, t', r) = partition x t
+
+delete :: (Ord a, Eq a) => a -> (Int, BTree a) -> (Int, BTree a)
+delete x (d, t) = fixRoot (d, del x t) where
+    del x t@(BTree ks ts)
+      | null ts = BTree (L.delete x ks) []
+      | otherwise = let (l, t', r@(ks', ts')) = partition x t in
+          if (not $ null ks') && (x == head ks') then
+            let k' = max' t' in balance d l (del k' t') (k':(tail ks'), ts')
+          else balance d l (del x t') r
+
+xs = [-68,-1,-56,-2,-3,-4,-58,-5,-6,-7,0,-57,-59,1,2,3,4,-55]
+-- k = -55
+-- d = 4
+tr = snd $ fromList 4 xs
+tr' = snd $ delete (-55) $ fromList 4 xs
+
 
 fixRoot (d, BTree [] [t]) = (d, t)
 fixRoot (d, t) | full d t  = let (t1, k, t2) = split d t in
@@ -95,6 +100,15 @@ balance d (ks1, ts1) t (ks2, ts2)
                                       (tail ks2, tail ts2)
            | otherwise = t
 
+-- lookup :: (Ord a)=> a -> BTree a -> Maybe (BTree a, Int)
+-- lookup tr@(BTree ks cs _) k
+--     | matchFirst k $ drop len ks = Just (tr, len)
+--     | otherwise = if null cs then Nothing
+--                   else lookup (cs !! len) k
+--     where
+--       matchFirst x (y:_) = x==y
+--       matchFirst x _ = False
+--       len = length $ filter (<k) ks
 
 fromList d xs = foldr insert (d, empty) xs
 
@@ -118,32 +132,19 @@ prop_order xs = (L.sort $ L.nub xs) == (toList $ snd $ fromList d xs) where
 prop_insert :: [Int] -> Bool
 prop_insert xs = isBTree d 0 $ snd $ fromList d xs where d = degOf xs
 
+prop_delete :: [Int] -> Int -> Bool
+prop_delete xs x = (L.sort $ L.delete x ys) ==
+                   (toList $ snd $ delete x $ fromList d ys) where
+  ys = L.nub xs
+  d = degOf xs
+
+prop_del_balance :: [Int] -> Int -> Bool
+prop_del_balance xs x = isBTree d 0 $ snd $ delete x $ fromList d ys where
+  ys = L.nub xs
+  d = degOf xs
+
 testAll = do
   quickCheck prop_order
   quickCheck prop_insert
-
--- testInsert = do
---   putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZ" 3
---   --testInsertV "GMPXACDEJKNORSTUVYZ" 3
---   putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZ" 2
---   --testInsertV "GMPXACDEJKNORSTUVYZ" 2
---   putStrLn $ toString $ listToBTree "GMPXACDEJKNORSTUVYZBFHIQW" 3
-
--- Verbose
--- testInsertV lst t =
---   mapM_ (\x->putStrLn $ toString $ listToBTree x t) (tail $ L.reverse $ L.tails lst)
-
--- testDelete = foldM_ delShow (listToBTree "GMPXACDEJKNORSTUVYZBFHIQW" 3) "EGAMU" where
---     delShow tr x = do
---       let tr' = delete tr x
---       putStrLn $ "delete "++(show x)
---       putStrLn $ toString tr'
---       return tr'
-
--- testSearch = mapM_ (showSearch (listToBTree lst 3)) $ lst++"L"
---     where
---       showSearch tr x = do
---         case search tr x of
---           Just (_, i) -> putStrLn $ "found" ++ (show x)
---           Nothing -> putStrLn $ "not found" ++ (show x)
---       lst = "GMPXACDEJKNORSTUVYZBFHIQW"
+  quickCheck prop_delete
+  quickCheck prop_del_balance
