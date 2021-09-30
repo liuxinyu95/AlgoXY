aa626e613b783d109e44af586272707be09c4fb3
Xinyu LIU <liuxinyu95@gmail.com>
2021-10-01 07:59:18 +0800

    Rewrite b-tree, split (EN)

--- datastruct/tree/B-tree/src/btree.py
+++ datastruct/tree/B-tree/src/btree.py
@@ -19,59 +19,59 @@
 TREE_2_3_4 = 2 #by default, create 2-3-4 tree
 
 class BTree:
-    def __init__(self, t=TREE_2_3_4):
-        self.t = t
-        self.keys = [] #self.data = ...
-        self.children = []
+    def __init__(self, d = TREE_2_3_4):
+        self.deg = d     # degree
+        self.keys = []   # self.data = ...
+        self.subtrees = []
 
 def is_leaf(t):
-    return t.children == []
+    return t.subtrees == []
 
 def is_full(node):
-    return len(node.keys) >= 2 * node.t - 1
+    return len(node.keys) >= 2 * node.deg - 1
 
 def can_remove(tr):
-    return len(tr.keys) >= tr.t
+    return len(tr.keys) >= tr.deg
 
 def replace_key(tr, i, k):
     tr.keys[i] = k
     return k
 
-def merge_children(tr, i):
-    #merge children[i] and children[i+1] by pushing keys[i] down
-    tr.children[i].keys += [tr.keys[i]] + tr.children[i+1].keys
-    tr.children[i].children += tr.children[i+1].children
+def merge_subtrees(tr, i):
+    #merge subtrees[i] and subtrees[i+1] by pushing keys[i] down
+    tr.subtrees[i].keys += [tr.keys[i]] + tr.subtrees[i + 1].keys
+    tr.subtrees[i].subtrees += tr.subtrees[i + 1].subtrees
     tr.keys.pop(i)
-    tr.children.pop(i+1)
+    tr.subtrees.pop(i + 1)
 
-def split_child(node, i):
-    t = node.t
-    x = node.children[i]
-    y = BTree(t)
-    node.keys.insert(i, x.keys[t-1])
-    node.children.insert(i+1, y)
-    y.keys = x.keys[t:]
-    x.keys = x.keys[:t-1]
+def split(node, i):
+    deg = node.deg
+    x = node.subtrees[i]
+    y = BTree(deg)
+    node.keys.insert(i, x.keys[deg - 1])
+    node.subtrees.insert(i + 1, y)
+    y.keys = x.keys[deg : ]
+    x.keys = x.keys[ : deg - 1]
     if not is_leaf(x):
-        y.children = x.children[t:]
-        x.children = x.children[:t]
+        y.subtrees = x.subtrees[deg : ]
+        x.subtrees = x.subtrees[ : deg]
 
 # insertion
 def insert(tr, key):
     root = tr
     if is_full(root):
-        s = BTree(root.t)
-        s.children.insert(0, root)
-        split_child(s, 0)
+        s = BTree(root.deg)
+        s.subtrees.insert(0, root)
+        split(s, 0)
         root = s
     return insert_nonfull(root, key)
 
 def ordered_insert(lst, x):
     i = len(lst)
     lst.append(x)
-    while i>0 and lst[i]<lst[i-1]:
-        (lst[i-1], lst[i]) = (lst[i], lst[i-1])
-        i=i-1
+    while i > 0 and lst[i] < lst[i - 1]:
+        (lst[i - 1], lst[i]) = (lst[i], lst[i - 1])
+        i = i - 1
 
 def insert_nonfull(tr, key):
     if is_leaf(tr):
@@ -79,14 +79,14 @@ def insert_nonfull(tr, key):
         #disk_write(tr)
     else:
         i = len(tr.keys)
-        while i>0 and key < tr.keys[i-1]:
-            i = i-1
-        #disk_read(tr.children[i])
-        if is_full(tr.children[i]):
-            split_child(tr, i)
+        while i > 0 and key < tr.keys[i-1]:
+            i = i - 1
+        #disk_read(tr.subtrees[i])
+        if is_full(tr.subtrees[i]):
+            split(tr, i)
             if key>tr.keys[i]:
-                i = i+1
-        insert_nonfull(tr.children[i], key)
+                i = i + 1
+        insert_nonfull(tr.subtrees[i], key)
     return tr
 
 # deletion
@@ -98,17 +98,17 @@ def B_tree_delete(tr, key):
                 tr.keys.remove(key)
                 #disk_write(tr)
             else: # case 2 in CLRS
-                if can_remove(tr.children[i-1]): # case 2a
-                    key = replace_key(tr, i-1, tr.children[i-1].keys[-1])
-                    B_tree_delete(tr.children[i-1], key)
-                elif can_remove(tr.children[i]): # case 2b
-                    key = replace_key(tr, i-1, tr.children[i].keys[0])
-                    B_tree_delete(tr.children[i], key)
+                if can_remove(tr.subtrees[i-1]): # case 2a
+                    key = replace_key(tr, i-1, tr.subtrees[i-1].keys[-1])
+                    B_tree_delete(tr.subtrees[i-1], key)
+                elif can_remove(tr.subtrees[i]): # case 2b
+                    key = replace_key(tr, i-1, tr.subtrees[i].keys[0])
+                    B_tree_delete(tr.subtrees[i], key)
                 else: # case 2c
-                    merge_children(tr, i-1)
-                    B_tree_delete(tr.children[i-1], key)
+                    merge_subtrees(tr, i-1)
+                    B_tree_delete(tr.subtrees[i-1], key)
                     if tr.keys==[]: # tree shrinks in height
-                        tr = tr.children[i-1]
+                        tr = tr.subtrees[i-1]
             return tr
         elif key > tr.keys[i-1]:
             break
@@ -117,25 +117,25 @@ def B_tree_delete(tr, key):
     # case 3
     if is_leaf(tr):
         return tr #key doesn't exist at all
-    if not can_remove(tr.children[i]):
-        if i>0 and can_remove(tr.children[i-1]): #left sibling
-            tr.children[i].keys.insert(0, tr.keys[i-1])
-            tr.keys[i-1] = tr.children[i-1].keys.pop()
-            if not is_leaf(tr.children[i]):
-                tr.children[i].children.insert(0, tr.children[i-1].children.pop())
-        elif i<len(tr.children) and can_remove(tr.children[i+1]): #right sibling
-            tr.children[i].keys.append(tr.keys[i])
-            tr.keys[i]=tr.children[i+1].keys.pop(0)
-            if not is_leaf(tr.children[i]):
-                tr.children[i].children.append(tr.children[i+1].children.pop(0))
+    if not can_remove(tr.subtrees[i]):
+        if i>0 and can_remove(tr.subtrees[i-1]): #left sibling
+            tr.subtrees[i].keys.insert(0, tr.keys[i-1])
+            tr.keys[i-1] = tr.subtrees[i-1].keys.pop()
+            if not is_leaf(tr.subtrees[i]):
+                tr.subtrees[i].subtrees.insert(0, tr.subtrees[i-1].subtrees.pop())
+        elif i<len(tr.subtrees) and can_remove(tr.subtrees[i+1]): #right sibling
+            tr.subtrees[i].keys.append(tr.keys[i])
+            tr.keys[i]=tr.subtrees[i+1].keys.pop(0)
+            if not is_leaf(tr.subtrees[i]):
+                tr.subtrees[i].subtrees.append(tr.subtrees[i+1].subtrees.pop(0))
         else: # case 3b
             if i>0:
-                merge_children(tr, i-1)
+                merge_subtrees(tr, i-1)
             else:
-                merge_children(tr, i)
-    B_tree_delete(tr.children[i], key)
+                merge_subtrees(tr, i)
+    B_tree_delete(tr.subtrees[i], key)
     if tr.keys==[]: # tree shrinks in height
-        tr = tr.children[0]
+        tr = tr.subtrees[0]
     return tr
 
 def B_tree_search(tr, key):
@@ -150,7 +150,7 @@ def B_tree_search(tr, key):
         if key>tr.keys[-1]:
             i=i+1
         #disk_read
-        return B_tree_search(tr.children[i], key)
+        return B_tree_search(tr.subtrees[i], key)
 
 def B_tree_to_str(tr):
     res = "("
@@ -158,8 +158,8 @@ def B_tree_to_str(tr):
         res += ", ".join(tr.keys)
     else:
         for i in range(len(tr.keys)):
-            res+= B_tree_to_str(tr.children[i]) + ", " + tr.keys[i] + ", "
-        res += B_tree_to_str(tr.children[len(tr.keys)])
+            res+= B_tree_to_str(tr.subtrees[i]) + ", " + tr.keys[i] + ", "
+        res += B_tree_to_str(tr.subtrees[len(tr.keys)])
     res += ")"
     return res
 
@@ -201,18 +201,18 @@ class BTreeTest:
         t = 3
         tr = BTree(t)
         tr.keys=["P"]
-        tr.children=[BTree(t), BTree(t)]
-        tr.children[0].keys=["C", "G", "M"]
-        tr.children[0].children=[BTree(t), BTree(t), BTree(t), BTree(t)]
-        tr.children[0].children[0].keys=["A", "B"]
-        tr.children[0].children[1].keys=["D", "E", "F"]
-        tr.children[0].children[2].keys=["J", "K", "L"]
-        tr.children[0].children[3].keys=["N", "O"]
-        tr.children[1].keys=["T", "X"]
-        tr.children[1].children=[BTree(t), BTree(t), BTree(t)]
-        tr.children[1].children[0].keys=["Q", "R", "S"]
-        tr.children[1].children[1].keys=["U", "V"]
-        tr.children[1].children[2].keys=["Y", "Z"]
+        tr.subtrees=[BTree(t), BTree(t)]
+        tr.subtrees[0].keys=["C", "G", "M"]
+        tr.subtrees[0].subtrees=[BTree(t), BTree(t), BTree(t), BTree(t)]
+        tr.subtrees[0].subtrees[0].keys=["A", "B"]
+        tr.subtrees[0].subtrees[1].keys=["D", "E", "F"]
+        tr.subtrees[0].subtrees[2].keys=["J", "K", "L"]
+        tr.subtrees[0].subtrees[3].keys=["N", "O"]
+        tr.subtrees[1].keys=["T", "X"]
+        tr.subtrees[1].subtrees=[BTree(t), BTree(t), BTree(t)]
+        tr.subtrees[1].subtrees[0].keys=["Q", "R", "S"]
+        tr.subtrees[1].subtrees[1].keys=["U", "V"]
+        tr.subtrees[1].subtrees[2].keys=["Y", "Z"]
         print B_tree_to_str(tr)
         lst = ["F", "M", "G", "D", "B", "U"]
         reduce(self.__test_del__, lst, tr)
