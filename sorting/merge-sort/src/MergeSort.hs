e48a3b1f438b3b1558d3ab8f006476d8b2bfe1c9
Xinyu LIU <liuxinyu95@gmail.com>
2022-12-29 09:30:38 +0800

    Simplified halve implementation

--- sorting/merge-sort/src/MergeSort.hs
+++ sorting/merge-sort/src/MergeSort.hs
@@ -1,16 +1,16 @@
 -- MergeSort1.hs
 -- Copyright (C) 2012 Liu Xinyu (liuxinyu95@gmail.com)
--- 
+--
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
--- 
+--
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -31,19 +31,22 @@ msort xs = merge (msort as) (msort bs) where
 mergesort [] = []
 mergesort [x] = [x]
 mergesort xs = merge (mergesort as) (mergesort bs) where
-    (as, bs) = split xs
+    (as, bs) = halve xs
 
-split [] = ([], [])
-split [x] = ([x], [])
-split (x:y:xs) = (x:xs', y:ys') where (xs', ys') = split xs
+halve = foldr f ([], []) where
+  f x (as, bs) = (bs, x : as)
+
+-- split [] = ([], [])
+-- split [x] = ([x], [])
+-- split (x:y:xs) = (x:xs', y:ys') where (xs', ys') = split xs
 
 merge xs [] = xs
 merge [] ys = ys
 merge (x:xs) (y:ys) | x <= y = x : merge xs (y:ys)
                     | x >  y = y : merge (x:xs) ys
-                               
+
 -- Bottom up version
--- Based on: Chris Okasaki. ``Purely functional data structures'' 6.4.3. 
+-- Based on: Chris Okasaki. ``Purely functional data structures'' 6.4.3.
 bmsort = sort' . map (\x->[x])
 
 sort' [] = []
@@ -61,7 +64,7 @@ bmsort' = foldl merge [] . map (\x->[x]) where
 --merge :: (Ord a)=>([a], Int)->([a], Int) -> ([a], Int)
 merge' ([], n) (ys, m) = (ys, n+m)
 merge' (xs, n) ([], m) = (xs, n+m)
-merge' ((x:xs), n) ((y:ys), m) = 
+merge' ((x:xs), n) ((y:ys), m) =
     if x< y then let (xs', n') = merge' (xs, n) ((y:ys), m) in (x:xs', n')
     else let (xs', n') = merge' ((x:xs), n) (ys, m) in (y:xs', n'+1)
 
