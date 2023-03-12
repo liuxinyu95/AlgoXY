46b12052e2dd79c69baa9e25f18bcabd3e4875c9
Xinyu LIU <liuxinyu95@gmail.com>
2023-03-12 17:29:29 +0800

    Refined Splay tree implementation

--- datastruct/heap/binary-heap/src/SplayHeap.hs
+++ datastruct/heap/binary-heap/src/SplayHeap.hs
@@ -28,40 +28,40 @@ data STree a = E -- Empty
              | Node (STree a) a (STree a) -- left, element, right
                deriving (Eq, Show)
 
--- partition the tree in two parts based on a pivot value, 
+-- partition the tree in two parts based on a pivot value,
 --  less part contains all elements < pivot
 --  bigger part contains all elements >= pivot
-partition :: (Ord a) => STree a -> a -> (STree a, STree a)
-partition E _ = (E, E)
-partition t@(Node l x r) y 
-    | x < y = 
+partition :: (Ord a) => a -> STree a -> (STree a, STree a)
+partition _ E = (E, E)
+partition y t@(Node l x r)
+    | x < y =
         case r of
           E -> (t, E)
-          Node l' x' r' -> 
-              if x' < y then 
-                  let (small, big) = partition r' y in
+          Node l' x' r' ->
+              if x' < y then
+                  let (small, big) = partition y r' in
                   (Node (Node l x l') x' small, big)
-              else 
-                  let (small, big) = partition l' y in
+              else
+                  let (small, big) = partition y l' in
                   (Node l x small, Node big x' r')
-    | otherwise = 
+    | otherwise =
         case l of
           E -> (E, t)
           Node l' x' r' ->
               if y < x' then
-                  let (small, big) = partition l' y in
+                  let (small, big) = partition y l' in
                   (small, Node l' x' (Node r' x r))
               else
-                  let (small, big) = partition r' y in
+                  let (small, big) = partition y r' in
                   (Node l' x' small, Node big x r)
-                    
-insert :: (Ord a) => STree a -> a -> STree a
-insert t x = Node small x big where (small, big) = partition t x
+
+insert :: (Ord a) => a -> STree a -> STree a
+insert x t = Node small x big where (small, big) = partition x t
 
 merge :: (Ord a) => STree a -> STree a -> STree a
 merge E t = t
 merge (Node l x r) t = Node (merge l l') x (merge r r')
-    where (l', r') = partition t x
+    where (l', r') = partition x t
 
 findMin :: STree a -> a
 findMin (Node E x _) = x
@@ -73,41 +73,41 @@ deleteMin (Node (Node E x' r') x r) = Node r' x r
 deleteMin (Node (Node l' x' r') x r) = Node (deleteMin l') x' (Node r' x r)
 
 -- splay by pattern matching
-splay :: (Eq a) => STree a -> a ->STree a
+splay :: (Eq a) => a -> STree a -> STree a
 -- zig-zig
-splay t@(Node (Node (Node a x b) p c) g d) y =
+splay y t@(Node (Node (Node a x b) p c) g d) =
     if x == y then Node a x (Node b p (Node c g d)) else t
-splay t@(Node a g (Node b p (Node c x d))) y =
+splay y t@(Node a g (Node b p (Node c x d))) =
     if x == y then Node (Node (Node a g b) p c) x d else t
 -- zig-zag
-splay t@(Node (Node a p (Node b x c)) g d) y =
+splay y t@(Node (Node a p (Node b x c)) g d) =
     if x == y then Node (Node a p b) x (Node c g d) else t
-splay t@(Node a g (Node (Node b x c) p d)) y =
+splay y t@(Node a g (Node (Node b x c) p d)) =
     if x == y then Node (Node a g b) x (Node c p d) else t
 -- zig
-splay t@(Node (Node a x b) p c) y = if x == y then Node a x (Node b p c) else t
-splay t@(Node a p (Node b x c)) y = if x == y then Node (Node a p b) x c else t
+splay y t@(Node (Node a x b) p c) = if x == y then Node a x (Node b p c) else t
+splay y t@(Node a p (Node b x c)) = if x == y then Node (Node a p b) x c else t
 -- otherwise
-splay t _ = t
+splay _ t = t
 
 -- insert by using pattern matching
-insert' :: (Ord a) => STree a -> a -> STree a
-insert' E y = Node E y E
-insert' (Node l x r) y 
-    | x > y     = splay (Node (insert' l y) x r) y
-    | otherwise = splay (Node l x (insert' r y)) y
+insert' :: (Ord a) => a -> STree a -> STree a
+insert' y E = Node E y E
+insert' y (Node l x r)
+    | x > y     = splay y (Node (insert' y l) x r)
+    | otherwise = splay y (Node l x (insert' y r))
 
-lookup' :: (Ord a) => STree a -> a -> STree a
-lookup' E _ = E
-lookup' t@(Node l x r) y
-    | x == y    = t
-    | x > y     = splay (Node (lookup' l y) x r) y
-    | otherwise = splay (Node l x (lookup' r y)) y
+member :: (Ord a) => a -> STree a -> (Bool, STree a)
+member _ E = (False, E)
+member y t@(Node l x r)
+    | x == y    = (True, t)
+    | x > y     = let (e, l') = member y l in (e, splay y (Node l' x r))
+    | otherwise = let (e, r') = member y r in (e, splay y (Node l x r'))
 
 -- general part for heap
 
 fromList :: (Ord a) => [a] -> STree a
-fromList xs = foldl insert E xs
+fromList = foldr insert E
 
 heapSort :: (Ord a) => [a] -> [a]
 heapSort = hsort . fromList where
@@ -121,22 +121,22 @@ testFromList = fromList [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
 testHeapSort = heapSort [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
 
 testFromList' = do
-  putStrLn $ show $ tolst [16, 14, 10, 8, 7, 9, 3, 2, 4, 1] 
+  putStrLn $ show $ tolst [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
   putStrLn $ show $ tolst [16, 10, 14, 8, 1, 7, 9, 3, 4, 2]
            where
-             tolst = foldl insert' E 
+             tolst = foldr insert' E
 
 
 toStr :: (Show a)=>(STree a) -> String
 toStr E = "."
-toStr (Node l x r) = "(" ++ (toStr l) ++ " " ++ 
+toStr (Node l x r) = "(" ++ (toStr l) ++ " " ++
                      (show x) ++ " " ++ (toStr r) ++ ")"
 
 
 --testSplay :: IO (STree Int)
 testSplay = do
   xs <- sequence (replicate 1000 (randomRIO(1, 10)))
-  putStrLn $ show (foldl lookup' t xs)
-  putStrLn $ show $ toStr (foldl lookup' t xs) -- in-order display
-      where 
-        t = foldl insert' (E::STree Int) [1..10]
+  putStrLn $ show (foldr ((snd .) . member) t xs)
+  putStrLn $ show $ toStr (foldr ((snd .) . member) t xs) -- in-order display
+      where
+        t = foldr insert' (E::STree Int) [1..10]
