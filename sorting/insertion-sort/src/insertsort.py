6a27458bce3d786363eeafa38a03c8a65fc85c11
Xinyu LIU <liuxinyu95@gmail.com>
2023-03-18 07:56:42 +0800

    Added answers to the insertion sort (insert fro left)

--- sorting/insertion-sort/src/insertsort.py
+++ sorting/insertion-sort/src/insertsort.py
@@ -2,17 +2,17 @@
 
 # insertsort.py
 # Copyright (C) 2012 Liu Xinyu (liuxinyu95@gmail.com)
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -33,7 +33,7 @@ def isort(xs):
         xs[j+1] = x
     return xs
 
-# verbose version
+# explicit insert function
 def isort1(xs):
     ys = []
     for x in xs:
@@ -43,12 +43,30 @@ def isort1(xs):
 def insert(xs, x):
     xs.append(x)
     i = len(xs) - 1
-    while i>0 and xs[i] < xs[i-1]:
+    while i > 0 and xs[i] < xs[i-1]:
         xs[i], xs[i-1] = xs[i-1], xs[i] #swap
         i = i - 1
 
+def isort4(xs):
+    ys = []
+    for x in xs:
+        insertl(ys, x)
+    return ys
+
+# insert from left
+def insertl(xs, x):
+    n = len(xs)
+    xs.append(x)
+    i = 0
+    while i < n and xs[i] < x:
+        i = i + 1
+    while i < n:
+        xs[n] = xs[n - 1]
+        n = n - 1
+    xs[i] = x
+
 # improvement 1
-#  Using binary search for insertion, 
+#  Using binary search for insertion,
 #    Compare O(N * lg N)
 #    Move O(N^2)
 
@@ -68,7 +86,7 @@ def binary_search(xs, x):
     l = 0
     u = len(xs)
     while l < u:
-        m = (l+u)/2
+        m = (l + u) // 2
         if xs[m] == x:
             return m # find a duplicated element
         elif xs[m] < x:
@@ -104,7 +122,7 @@ def reorder(xs, next):
     return ys
 
 def test():
-    for _ in xrange(100):
+    for _ in range(100):
         n = random.randint(0, 100)
         xs = [random.randint(0, n) for _ in range(n)]
         ys = sorted(xs)
@@ -112,14 +130,16 @@ def test():
         assert(isort1(list(xs)) == ys)
         assert(isort2(list(xs)) == ys)
         assert(isort3(list(xs)) == ys)
+        assert(isort4(list(xs)) == ys)
+    print("done")
 
 # internal use only, need remove
 def assertEqual(xs, ys):
     if not xs == ys:
-        print "xs", xs
-        print "ys", ys
+        print("xs", xs)
+        print("ys", ys)
         exit(1)
-    
+
 if __name__ == "__main__":
     #print isort3([5,1,2,4,3,3])
     test()
