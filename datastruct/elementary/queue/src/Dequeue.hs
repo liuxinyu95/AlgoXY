64d2ca512169b66265f539fc329b65cf4887727f
Xinyu LIU <liuxinyu95@gmail.com>
2023-04-21 16:54:53 +0800

    Simplifed dequeue implementation

--- datastruct/elementary/queue/src/Dequeue.hs
+++ datastruct/elementary/queue/src/Dequeue.hs
@@ -2,29 +2,26 @@ module Dequeue where
 
 import Test.QuickCheck
 
-data DeQueue a = DQ [a] Int [a] Int
+empty = ([], [])
 
-empty = DQ [] 0 [] 0
+isEmpty ([], []) = True
+isEmpty _ = False
 
-isEmpty (DQ _ m _ n) = m == 0 && n == 0
+pushL x (f, r) = (x:f, r)
 
-pushL x (DQ f m r n) = DQ (x:f) (m + 1) r n
+pushR (f, r) x = (f, x:r)
 
-pushR (DQ f m r n) x = DQ f m (x:r) (n + 1)
+popL ([], []) = (Nothing, empty)
+popL ([],  r) = let (as, bs) = splitAt (length r `div` 2) r in
+                  popL (reverse bs, as)
+popL (x:f, r) = (Just x, (f, r))
 
-popL q@(DQ [] _ [] _) = (Nothing, q)
-popL (DQ [] _ r n) = let m = n `div` 2
-                         (as, bs) = splitAt m r in
-                       popL (DQ (reverse bs) (n - m) as m)
-popL (DQ (x:f) m r n) = (Just x, DQ f (m - 1) r n)
+popR ([], []) = (empty, Nothing)
+popR (f , []) = let (as, bs) = splitAt (length f `div` 2) f in
+                  popR (as, reverse bs)
+popR (f, x:r) = ((f, r), Just x)
 
-popR q@(DQ [] _ [] _) = (q, Nothing)
-popR (DQ f m [] _) = let n = m `div` 2
-                         (as, bs) = splitAt n f in
-                       popR (DQ as n (reverse bs) (m - n))
-popR (DQ f m (x:r) n) = (DQ f m r (n - 1), Just x)
-
-toList (DQ f _ r _) = f ++ reverse r
+toList (f, r) = f ++ reverse r
 
 prop_queue :: [Int] -> Bool
 prop_queue xs = (toList as) == bs where
